ff3913b442466f71b9b9a5e66b9990a27c4f4471
denispelli <denis.pelli@nyu.edu>
2015-11-30 02:26:56 -0500

    Document the key parameters that users are likely to use.

--- runCriticalSpacing.m
+++ runCriticalSpacing.m
@@ -1,19 +1,26 @@
 % MATLAB script to run CriticalSpacing.m
 % Copyright 2015, Denis G. Pelli, denis.pelli@nyu.edu
 clear o
+% I don't know yet how many trials are needed. Note that each "trial"
+% is one presentation. Thus testing repeated letters, there are two
+% targets, and two responses per "trial". You'll probably want twice as
+% many "trials" when testing with a single target. I think 40 responses
+% will give an accurate threshold estimate. Perhaps 20 would be enough.
+% When there are two targets (repeatedLetters==1) we thus set trials=20.
 o.trials=20; % Number of presentations (two response per presentation) for the threshold estimate.
 % The viewing distance is set here. The program will try to use what you
 % selected, otherwise it will abort and tell you the minimum viewing
 % distance that you need. You must then modify this file to set the new
 % viewing distance. And, of course, move the screen to that distance.
 o.viewingDistanceCm=300;
+% You probably won't need to change any other parameters.
+o.repeatedLetters=1; % Repeated letter make the test immune to fixation errors.
 o.flipScreenHorizontally=0; % Set to 1 when using a mirror to achieve a long viewing distance.
 o.observer=''; % Ask for name at beginning of run, or
 % o.observer='Shivam'; % enter observer name here.
 o.useScreenCopyWindow=1; % Faster, but fails on some Macs. If your repeated-letters screen is incomplete, set this to 0.
 o.negativeFeedback=0;
 o.encouragement=1; % Randomly say good, very good, or nice after every trial.
-o.repeatedLetters=1; % Repeated letter make the test immune to fixation errors.
 o.fixationCrossDeg=0;
 o.useFractionOfScreen=0;
 o.thresholdParameter='spacing';
@@ -22,7 +29,7 @@ o.durationSec=inf; % duration of display of target and flankers
 o.measureBeta=0;
 o.task='identify';
 o.usePurring=1;
-minimumTargetPix=8;
+minimumTargetPix=8; % Make sure the letters are well rendered.
 o.alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 o.alphabet='DHKNORSVZ'; % for the Sloan alphabet
 o.targetFont='Sloan';
@@ -35,6 +42,7 @@ o(2).thresholdParameter='size';
 oRepeated=CriticalSpacing(o); % dual targets, repeated indefinitely
 % We retain the observer name obtained during the first run for use in the
 % second run.
+o.trials=2*o.trials;
 o(1).repeatedLetters=0;
 o(2).repeatedLetters=0;
 o(1).observer=oRepeated(1).observer;
