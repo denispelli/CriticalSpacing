1d43c5a74d50541fc23886301440e20242096d14
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-12-17 13:44:10 -0500

    test all keyboard interaction without running Screen

---
+++ lib/testResponse.m
@@ -0,0 +1,93 @@
+function testResponse
+% emulates CriticalSpacing procedure for response collection and processing
+KbName('UnifyKeyNames');
+RestrictKeysForKbCheck([]);
+Screen('Preference','SkipSyncTests',1);
+escapeKey=KbName('ESCAPE');
+spaceKey=KbName('space');
+
+oo.useSpeech=1;
+oo.speakEachLetter=1;
+oo.speakEncouragement=0;
+oo.beepPositiveFeedback=1;
+oo.beepNegativeFeedback=0;
+rightBeep=MakeBeep(2000,0.05,44100);
+rightBeep(end)=0;
+wrongBeep=MakeBeep(500,0.5,44100);
+wrongBeep(end)=0;
+purr=MakeBeep(140,1.0,44100);
+purr(end)=0;
+Snd('Open');
+
+condition = 1;
+oo.deviceIndex = -3;
+oo.alphabet='!7ij:()[]/|'; % Sloan alphabet, excluding C
+oo.borderLetter='!';
+
+stimulus=Shuffle(oo(condition).alphabet);
+stimulus=stimulus(1:3); % three random letters, all different.
+targets=stimulus(1:2);
+
+
+terminate=0;
+responseString='';
+for i=1:length(oo(condition).alphabet)
+  oo(condition).responseKeys(i)=KbName(oo(condition).alphabet(i));
+end
+disp('Targets are:');
+disp(targets);
+disp('Checking for the responseKeys list below:');
+disp(oo(condition).responseKeys);
+disp(KbName(oo.responseKeys));
+disp(' ');
+
+for i=1:length(targets)
+  while(1)
+    answer=GetKeypress(0,[escapeKey oo(condition).responseKeys],oo.deviceIndex,1);
+    answer=upper(answer);
+    if ~ismember(answer,responseString)
+      break
+    end
+  end
+  if oo(condition).speakEachLetter && oo(condition).useSpeech
+    Speak(answer);
+  end
+  
+  if streq(answer,'ESCAPE')
+    ListenChar(0);
+    ffprintf('*** Observer typed escape. Run terminated.\n');
+    terminate=1;
+    break;
+  end
+  
+  if ismember(answer,targets)
+    if oo(condition).beepPositiveFeedback
+      Snd('Play',rightBeep);
+    end
+  else
+    if oo(condition).beepNegativeFeedback
+      Snd('Play',wrongBeep);
+    end
+  end
+  responseString=[responseString answer];
+end
+
+if oo(condition).speakEncouragement && oo(condition).useSpeech && ~terminate
+  switch randi(3);
+    case 1
+      Speak('Good!');
+    case 2
+      Speak('Nice');
+    case 3
+      Speak('Very good');
+  end
+end
+%   if terminate
+%     break;
+%   end
+assert(length(targets)==length(responseString))
+responses=sort(targets)==sort(responseString);
+disp('responses sending to QUEST are:')
+disp(responses);
+
+end
