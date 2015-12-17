220137ad452f71decbd46a9824b6edbcbadd1d03
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-12-17 14:31:48 -0500

    drop legacy mode that supported GetChar()

--- lib/GetKeypress.m
+++ lib/GetKeypress.m
@@ -1,47 +1,34 @@
-function response = GetKeypress(isKbLegacy,enableKeys, deviceIndex, simulateGetChar)
-% Wait for keypress, and return the lowercase character, e.g. 'a' or '4',
-% or key name, e.g. 'left_shift'. We do not distinguish between pressing a
-% number key on the main or separate numeric keyboard; we just return the
-% one-digit number as a character. simulateGetChar ensures this behavoir.
-% If simulateGetChar is set to false, then full key codes are returned
-% instead, e.g. 'a' or '4$'.
-%
-% Originally called "checkResponse" written by Hormet Yiltiz, October 2015.
-% Renamed "GetKeypress" by Denis Pelli, November 2015.
+function response = GetKeypress(enableKeys, deviceIndex, simulateGetChar)
+  % Wait for keypress, and return the lowercase character, e.g. 'a' or '4',
+  % or key name, e.g. 'left_shift'. We do not distinguish between pressing a
+  % number key on the main or separate numeric keyboard; we just return the
+  % one-digit number as a character. simulateGetChar ensures this behavoir.
+  % If simulateGetChar is set to false, then full key codes are returned
+  % instead, e.g. 'a' or '4$'.
+  %
+  % Originally called "checkResponse" written by Hormet Yiltiz, October 2015.
+  % Renamed "GetKeypress" by Denis Pelli, November 2015.
 
-isDebug = 1;
+  isDebug = 1;
 
-if nargin == 0
-  isKbLegacy=0;
-end
-if nargin >= 2
-  % make sure enableKeys are cell strings
-  oldEnableKeys=RestrictKeysForKbCheck(enableKeys);
-  if isDebug; disp('Enabled keys list is:');disp(enableKeys);end
-end
-if nargin<3
-  deviceIndex=-3; % All keyboards and keypads.
-end
-if nargin<4
-  % simulate the bahavior of GetChar() -- only return the character response
-  % discarding the second character when two are captured
-  simulateGetChar = 1;
-end
-KbName('UnifyKeyNames'); % make sure this is set
+  if nargin >= 1
+    % make sure enableKeys are cell strings
+    oldEnableKeys=RestrictKeysForKbCheck(enableKeys);
+    if isDebug; disp('Enabled keys list is:');disp(enableKeys);end
+  end
+  if nargin<2
+    deviceIndex=-3; % All keyboards and keypads.
+  end
+  if nargin<3
+    % simulate the bahavior of GetChar() -- only return the character response
+    % discarding the second character when two are captured
+    simulateGetChar = 1;
+  end
+  KbName('UnifyKeyNames'); % make sure this is set
 
-while KbCheck; end
-response=0;
-if isKbLegacy
-  % Use GetChar, which is not supported on Windows? and Linux?
-  if isDebug; disp('Using legacy GetChar()!');end
-  ListenChar(2); % no echo
-  response=GetChar;
-  ListenChar(0); % flush
-  ListenChar; % normal
-
-else
+  while KbCheck; end
+  response=0;
   % use modern Kb* functions
-
   [secs, keyCode] = KbStrokeWait(deviceIndex); % we only need keyCode
   response = KbName(keyCode);
   if isDebug;fprintf('You pressed ==>%s<==\n', response);end
@@ -78,8 +65,9 @@ else
     while KbCheck;end
   end
 
-end
-if nargin >= 2
-  RestrictKeysForKbCheck(oldEnableKeys);
-end
+
+
+  if nargin >= 2
+    RestrictKeysForKbCheck(oldEnableKeys);
+  end
 end
