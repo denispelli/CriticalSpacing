bc80c4c52cf11b918a8e59262de4a93cf899b533
Hormet Yiltiz <hyiltiz@gmail.com>
2015-12-18 18:21:32 -0500

    plotting done

--- lib/plotAndSave.m
+++ lib/plotAndSave.m
@@ -1,13 +1,25 @@
 letters = '123456789';
 
 for i=1:1%numel(letters)
-%     figure('Units', 'pixels', 'Position', [0 0 800 800]);
-figure('Units', 'pixels', 'Position', [0 0 1200 1200]);
-    text(0.2,0.5, letters(1), 'FontName', 'Sloan','FontUnits', 'pixels', 'FontSize', 800);
+    figure('Units', 'pixels', 'Position', [0 0 600 600]);
+    text(0,300, letters(i), 'Units', 'pixels', 'FontName', 'Sloan','FontUnits', 'pixels', 'FontSize', 600);
     box off
     axis off
-%     box off
-    print this.tiff -dtiffnocompression;
-    ss = imread('this.tiff');
-   % close;
+    set(gca,'XTick',[]) % Remove the ticks in the x axis!
+    set(gca,'YTick',[]) % Remove the ticks in the y axis
+    %     print this.tiff -dtiffnocompression -r0 -noui -opengl;
+    %     saveas(gcf, 'this.tiff',' -tiffn');
+    %     export_fig this.png;
+    F = getframe(gcf);
+    [ss, Map] = frame2im(F);
+    
+%     ss = imread('this.png');
+    close;
+    
+    
+    % we just use the ss here, dont actually need to plot the stuff as
+    % below
+    figure;image(ss);pause;close; % see the coordinates, for programming the clipping
+    figure;imshow(ss);pause;close; % this is what we see
+    
 end
