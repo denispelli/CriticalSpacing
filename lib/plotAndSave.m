24675a10a736941014d73aae2317ae40ea35703c
Hormet Yiltiz <hyiltiz@gmail.com>
2015-12-18 17:56:42 -0500

    better

--- lib/plotAndSave.m
+++ lib/plotAndSave.m
@@ -1,9 +1,13 @@
 letters = '123456789';
 
-for i=1:numel(letters)
-    figure('Position', [0 0 800 800]);
-    text(0,0, letters(1));
+for i=1:1%numel(letters)
+%     figure('Units', 'pixels', 'Position', [0 0 800 800]);
+figure('Units', 'pixels', 'Position', [0 0 1200 1200]);
+    text(0.2,0.5, letters(1), 'FontName', 'Sloan','FontUnits', 'pixels', 'FontSize', 800);
+    box off
+    axis off
+%     box off
     print this.tiff -dtiffnocompression;
     ss = imread('this.tiff');
-    close;
+   % close;
 end
