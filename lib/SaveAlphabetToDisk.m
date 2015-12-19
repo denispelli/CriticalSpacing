b03b21089e39c37ac89779850db5e2cb8a0eb7a0
denispelli <denis.pelli@nyu.edu>
2015-12-19 20:04:43 +0000

    SaveAlphabetToDisk MATLAB option working

--- lib/SaveAlphabetToDisk.m
+++ lib/SaveAlphabetToDisk.m
@@ -1,137 +1,185 @@
-% function savedAlphabet=SaveAlphabetToDisk(o)
-% if nargin<1
-if 1
-    o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
-    o.validKeys = {'d','h','k','n','o','r','s','v','z'};
-    o.borderLetter='X';
-    o.targetFont='Sloan';
-    % o.targetFont='Gotham Cond SSm Medium';
-    o.useMATLABFontRendering=1;
+function savedAlphabet=SaveAlphabetToDisk(o)
+if nargin<1
+   sca
+   
+   o.targetFont='Sloan';
+   o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
+   o.validKeys = {'d','h','k','n','o','r','s','v','z'};
+   o.borderLetter='X';
+   
+   o.targetFont='Gotham Cond SSm Medium';
+   o.targetFont='Gotham Cond SSm Black';
+   o.alphabet='123456789';
+   o.borderLetter='0';
+   o.validKeys = {'1!','2@','3#','4$','5%','6^','7&','8*','9('};
+
+   o.useMATLABFontRendering=1;
+   showProgress=1;
+   useWindow=0;
 end
 filename=fullfile(fileparts(mfilename('fullpath')),'savedAlphabet');
 try
-    load(filename,'savedAlphabet');
+   load(filename,'savedAlphabet');
 catch
-    clear savedAlphabet
+   clear savedAlphabet
 end
 if exist('savedAlphabet','var')
-    ia=length(savedAlphabet)+1;
+   for ia=1:length(savedAlphabet)
+      match=streq(savedAlphabet(ia).targetFont,o.targetFont);
+      if match
+         savedAlphabet(ia).letters='';
+         savedAlphabet(ia).validKeys={};
+         savedAlphabet(ia).bounds={};
+         savedAlphabet(ia).images={};
+         savedAlphabet(ia).rect=[];
+         savedAlphabet(ia).dx=[];
+         savedAlphabet(ia).width=[];
+         savedAlphabet(ia).meanOverMaxTargetWidth=[];
+         break;
+      end;
+   end
+   if ~match
+      ia=length(savedAlphabet)+1;
+   end
 else
-    ia=1;
+   ia=1;
 end
 letters=[o.alphabet o.borderLetter];
 letterPix=512;
 savedAlphabet(ia).targetFont=o.targetFont;
 savedAlphabet(ia).letters=letters;
 savedAlphabet(ia).validKeys=o.validKeys;
-black=0;
-white=255;
-[window,windowRect]=Screen('OpenWindow',0,255,[0 0 256 256]);
-% Screen('preference','ConserveVRAM',2);
-Screen('FillRect',window);
-Screen('Flip',window);
-[scratchWindow,scratchRect]=Screen('OpenOffscreenWindow',window,[],2*[0 0 letterPix letterPix]);
+if useWindow
+   black=0;
+   white=255;
+   [window,windowRect]=Screen('OpenWindow',0,255,[0 0 512 512]);
+   % Screen('preference','ConserveVRAM',2);
+   Screen('FillRect',window);
+   Screen('Flip',window);
+   [scratchWindow,scratchRect]=Screen('OpenOffscreenWindow',window,[],2*[0 0 letterPix letterPix]);
+end
 % Set font.
 if IsOSX
-    fontInfo=FontInfo('Fonts');
-    % Match full name, including style.
-    hits=streq({fontInfo.name},o.targetFont);
-    if sum(hits)<1
-        % Match family name, omitting style.
-        hits=streq({fontInfo.familyName},o.targetFont);
-    end
-    if sum(hits)==0
-        error('The o.targetFont "%s" is not available. Please install it.',o.targetFont);
-    end
-    if sum(hits)>1
-        error('Multiple fonts with name "%s".',o.targetFont);
-    end
-    o.targetFontNumber=fontInfo(hits).number;
-    Screen('TextFont',window,o.targetFontNumber);
-    [font,number]=Screen('TextFont',window);
-    if ~(number==o.targetFontNumber)
-        error('The o.targetFont "%s" is not available. Please install it.',o.targetFont);
-    end
+   fontInfo=FontInfo('Fonts');
+   % Match full name, including style.
+   hits=streq({fontInfo.name},o.targetFont);
+   if sum(hits)<1
+      % Match family name, omitting style.
+      hits=streq({fontInfo.familyName},o.targetFont);
+   end
+   if sum(hits)==0
+      error('The o.targetFont "%s" is not available. Please install it.',o.targetFont);
+   end
+   if sum(hits)>1
+      error('Multiple fonts with name "%s".',o.targetFont);
+   end
+   o.targetFontNumber=fontInfo(hits).number;
+   if useWindow
+      Screen('TextFont',window,o.targetFontNumber);
+      [font,number]=Screen('TextFont',window);
+      if ~(number==o.targetFontNumber)
+         error('The o.targetFont "%s" is not available. Please install it.',o.targetFont);
+      end
+   end
 else
-    o.targetFontNumber=[];
-    Screen('TextFont',window,o.targetFont);
-    font=Screen('TextFont',window);
-    if ~streq(font,o.targetFont)
-        error('The o.targetFont "%s" is not available. Please install it.',o.targetFont);
-    end
+   if useWindow
+      o.targetFontNumber=[];
+      Screen('TextFont',window,o.targetFont);
+      font=Screen('TextFont',window);
+      if ~streq(font,o.targetFont)
+         error('The o.targetFont "%s" is not available. Please install it.',o.targetFont);
+      end
+   end
 end
-if ~isempty(o.targetFontNumber)
-    Screen('TextFont',scratchWindow,o.targetFontNumber);
-    [font,number]=Screen('TextFont',scratchWindow);
-    assert(number==o.targetFontNumber);
-else
-    Screen('TextFont',scratchWindow,o.targetFont);
-    font=Screen('TextFont',scratchWindow);
-    assert(streq(font,o.targetFont));
+if useWindow
+   if ~isempty(o.targetFontNumber)
+      Screen('TextFont',scratchWindow,o.targetFontNumber);
+      [font,number]=Screen('TextFont',scratchWindow);
+      assert(number==o.targetFontNumber);
+   else
+      Screen('TextFont',scratchWindow,o.targetFont);
+      font=Screen('TextFont',scratchWindow);
+      assert(streq(font,o.targetFont));
+   end
+   Screen('TextSize',scratchWindow,letterPix);
+   for i=1:length(letters)
+      lettersInCells{i}=letters(i);
+   end
+   bounds=TextBounds(scratchWindow,lettersInCells,1);
+   savedAlphabet(ia).rect=OffsetRect(bounds,-bounds(1),-bounds(2));
+   for i=1:length(letters)
+      letterBounds=TextBounds(scratchWindow,letters(i),1);
+      desiredBounds=CenterRect(letterBounds,bounds);
+      savedAlphabet(ia).dx(i)=desiredBounds(1)-letterBounds(1);
+      savedAlphabet(ia).width(i)=RectWidth(letterBounds);
+   end
+   savedAlphabet(ia).meanOverMaxTargetWidth=mean([savedAlphabet(ia).width])/RectWidth(bounds);
 end
-Screen('TextSize',scratchWindow,letterPix);
 for i=1:length(letters)
-    lettersInCells{i}=letters(i);
-end
-bounds=TextBounds(scratchWindow,lettersInCells,1);
-savedAlphabet(ia).rect=OffsetRect(bounds,-bounds(1),-bounds(2));
-for i=1:length(letters)
-    letterBounds=TextBounds(scratchWindow,letters(i),1);
-    desiredBounds=CenterRect(letterBounds,bounds);
-    savedAlphabet(ia).dx(i)=desiredBounds(1)-letterBounds(1);
-    savedAlphabet(ia).width(i)=RectWidth(letterBounds);
-end
-savedAlphabet(ia).meanOverMaxTargetWidth=mean([savedAlphabet(ia).width])/RectWidth(bounds);
-for i=1:length(letters)
-    if o.useMATLABFontRendering
-        figure('Units', 'pixels', 'Position', [0 0 1024 1024]);
-        text(0,256, letters(i), 'Units', 'pixels', 'FontName', 'Sloan','FontUnits', 'pixels', 'FontSize', 512);
-        box off
-        axis off
-        set(gca,'XTick',[]) % Remove the ticks in the x axis!
-        set(gca,'YTick',[]) % Remove the ticks in the y axis
-        F = getframe(gcf);
-        letterImage= frame2im(F);
-        close;
-        letterImage=letterImage(:,:,2);
-        savedAlphabet(ia).bounds{i}=ImageBounds(letterImage,letterImage(1,1));
-        savedAlphabet(ia).images{i}=letterImage;
-    Screen('PutImage',window,letterImage);
-    Screen('Flip',window);
-    WaitSecs(0.5);
-    else
-        Screen('FillRect',scratchWindow,white);
-        %     Screen('DrawText',window,letters(i),-bounds(1)+savedAlphabet(ia).dx(i),-bounds(2),black,white,1);
-        Screen('DrawText',scratchWindow,letters(i),-bounds(1)+savedAlphabet(ia).dx(i),-bounds(2),black,white,1);
-        %     Screen('DrawingFinished',scratchWindow); % Might make GetImage more reliable. Suggested by Mario Kleiner.
-        %     Screen('DrawTexture',window,scratchWindow,savedAlphabet(ia).rect,windowRect);
-        %     letterImage=Screen('GetImage',scratchWindow,bounds);
-        letterImage=Screen('GetImage',window,bounds,'drawBuffer');
-        %    imshow(letterImage);
-        %    letterImage=Screen('GetImage',scratchWindow,bounds,'drawBuffer');
-        savedAlphabet(ia).images{i}=letterImage;
-        %    savedAlphabet(ia).images{i}=letterImage(:,:,2);
-        %    imshow(savedAlphabet(ia).images{i});
-        %    Screen('DrawText',window,letters(i),-bounds(1)+savedAlphabet(ia).dx(i),-bounds(2),black,white,1);
-    end
-    Screen('PutImage',window,letterImage,OffsetRect(bounds,400,400));
-    Screen('Flip',window);
-    WaitSecs(0.5);
+   if o.useMATLABFontRendering
+      figure('Units','pixels','Position',[0 0 1024 1024]);
+      h=text(0,256,letters(i),'Units','pixels','FontName',o.targetFont,'FontUnits','pixels','FontSize',512);
+      box off
+      axis off
+      set(gca,'Color',[1 1 1],'XTick',[],'YTick',[]); % White background, no ticks.
+      letterImage=frame2im(getframe(gcf));
+      close; % figure
+      letterImage=letterImage(:,:,2); % Convert RGB to grayscale.
+      savedAlphabet(ia).bounds{i}=ImageBounds(letterImage,letterImage(1,1));
+      savedAlphabet(ia).images{i}=letterImage;
+   else
+      if useWindow
+         Screen('FillRect',scratchWindow,white);
+         %     Screen('DrawText',window,letters(i),-bounds(1)+savedAlphabet(ia).dx(i),-bounds(2),black,white,1);
+         Screen('DrawText',scratchWindow,letters(i),-bounds(1)+savedAlphabet(ia).dx(i),-bounds(2),black,white,1);
+         %     Screen('DrawingFinished',scratchWindow); % Might make GetImage more reliable. Suggested by Mario Kleiner.
+         %     Screen('DrawTexture',window,scratchWindow,savedAlphabet(ia).rect,windowRect);
+         %     letterImage=Screen('GetImage',scratchWindow,bounds);
+         letterImage=Screen('GetImage',window,bounds,'drawBuffer');
+         %    imshow(letterImage);
+         %    letterImage=Screen('GetImage',scratchWindow,bounds,'drawBuffer');
+         savedAlphabet(ia).images{i}=letterImage;
+         %    savedAlphabet(ia).images{i}=letterImage(:,:,2);
+         %    imshow(savedAlphabet(ia).images{i});
+         %    Screen('DrawText',window,letters(i),-bounds(1)+savedAlphabet(ia).dx(i),-bounds(2),black,white,1);
+      end
+   end
+   if useWindow
+      Screen('PutImage',window,letterImage,OffsetRect(bounds,400,400));
+      Screen('Flip',window);
+      WaitSecs(0.5);
+   end
 end
 if o.useMATLABFontRendering
-    b=savedAlphabet(ia).bounds{1};
-    for i=1:length(letters)
-        b=UnionRect(b,savedAlphabet(ia).bounds{i})
-    end
-    for i=1:length(letters)
-        savedAlphabet(ia).images{i}=savedAlphabet(ia).images{i}(1+b(1):b(3),1+b(2):b(4));
-    end
-    savedAlphabet(ia).rect=RectOfMatrix(savedAlphabet(ia).images{1});
+   b=savedAlphabet(ia).bounds{1};
+   for i=1:length(letters)
+      b=UnionRect(b,savedAlphabet(ia).bounds{i});
+   end
+   for i=1:length(letters)
+      savedAlphabet(ia).images{i}=savedAlphabet(ia).images{i}(1+b(2):b(4),1+b(1):b(3));
+      %       size(savedAlphabet(ia).images{i})
+      savedAlphabet(ia).bounds{i}=OffsetRect(savedAlphabet(ia).bounds{i},-b(1),-b(2));
+      %       savedAlphabet(ia).bounds{i}
+      savedAlphabet(ia).rect=OffsetRect(b,-b(1),-b(2));
+      if showProgress
+         if useWindow
+            Screen('PutImage',window,savedAlphabet(ia).images{i});
+            Screen('FrameRect',window,0,savedAlphabet(ia).bounds{i});
+            Screen('Flip',window);
+            WaitSecs(0.5);
+         else
+            imshow(savedAlphabet(ia).images{i});
+         end
+      end
+      savedAlphabet(ia).rect=RectOfMatrix(savedAlphabet(ia).images{1});
+   end
 end
-Screen('Close',scratchWindow);
-Screen('Close',window);
+if useWindow
+   Screen('Close',scratchWindow);
+   Screen('Close',window);
+end
 filename=fullfile(fileparts(mfilename('fullpath')),'savedAlphabet');
 save(filename,'savedAlphabet');
-fprintf('Saved images of "%s" alphabet "%s".\n',o.targetFont,letters);
+fprintf('Saved images of "%s" alphabet "%s" in file "savedAlphabet".\n',o.targetFont,letters);
 fprintf('Done.\n');
 sca
