624b1cb2e9e86ec8bb1737aed2745eb031e1a001
denispelli <denis.pelli@nyu.edu>
2015-12-21 00:24:31 +0000

    update for Sarah & Silvia

--- runCriticalSpacing.m
+++ runCriticalSpacing.m
@@ -61,16 +61,17 @@ o.viewingDistanceCm=1000;
 % letters.
 o.flipScreenHorizontally=0; % Set to 1 when using a mirror.
 
-% For children, past investigators, including Jan Atkinson's Cambridge
-% Crowding Cards, have used symmetric letters HOTVX, so we provide that
-% option too. However, pilot testing indicates that it takes more trials
-% with 5 possible targets to get the same precision (SD) as with 9 possible
-% targets. Time is paramount, so we're sticking with the 9 Sloan letters
-% for the time being.
+% NOT RECOMMENDED BECAUSE THE SMALLER ALPHABET DOUBLES THE STANDARD ERROR
+% OF THE THRESHOLD ESTIMATE. For children, past investigators, including
+% Jan Atkinson's Cambridge Crowding Cards, have used symmetric letters
+% HOTVX, so we provide that option too. However, pilot testing indicates
+% that it takes more trials with 5 possible targets to get the same
+% precision (SD) as with 9 possible targets. Time is paramount, so we're
+% sticking with the 9 Sloan letters for the time being.
 % o.alphabet='HOTVX'; % alphabet of Cambridge Crowding Cards
 % o.borderLetter='N';
 
-% UNTESTED: Skinny letters are better for testing critical spacing.
+% UNTESTED: Skinny symbols might be better for testing critical spacing.
 % o.alphabet='7ij:()[]/|'; % bar-symbol alphabet
 % o.validKeys = {'7&','i','j',';:','9(','0)','[{',']}','/?','\|'};
 % o.borderLetter='!';
@@ -108,35 +109,14 @@ o.usePurring=0;
 % You don't need to change any of these parameters.
 o.observer=''; % Ask for name at beginning of run, or
 % o.observer='Shivam'; % enter observer name here.
-o.readAlphabetFromDisk=1; % 1 makes the program more portable.
 o.usePurring=0; % Play purring sound while awaiting user response.
 % o.radialOrTangential='tangential'; % vertically arranged flankers for single target
 o.radialOrTangential='radial'; % horizontally arranged flankers for single target
-o.negativeFeedback=0;
 o.fixationCrossDeg=0;
 o.durationSec=inf; % duration of display of target and flankers
 o.measureBeta=0;
 o.task='identify';
 o.minimumTargetPix=8; % Make sure the letters are well rendered.
-
-% For normal adults we use the restricted standard Sloan alphabet
-% (excluding C, which has been shown to be too similar to O).
-o.targetFont='Sloan';
-o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
-o.borderLetter='X';
-o.validKeys={'D','H','K','N','O','R','S','V','Z'};
-
-% THESE FONTS ARE DESIGNED TO BE LEGIBLE AT SMALL SIZES.
-% o.targetFont='ClearviewText';
-% o.targetFont='Gotham Cond SSm Book';
-% o.targetFont='Gotham Cond SSm Medium';
-o.targetFont='Gotham Cond SSm Bold';
-% o.targetFont='Retina Micro';
-% o.targetFont='Sticks'; % Available only through savedAlphabet.
-o.alphabet='123456789'; 
-o.borderLetter='0';
-o.validKeys = {'1!','2@','3#','4$','5%','6^','7&','8*','9('};
-
 o.textFont='Calibri';
 o.fixationLocation='center';
 
@@ -148,6 +128,32 @@ o.frameTheTarget=0; % For debugging.
 o.useFractionOfScreen=0; % For debugging.
 o.printSizeAndSpacing=0; % For debugging.
 o.displayAlphabet=0; % For debugging.
+
+% CHOOSE FONT & ALPHABET
+% CURRENTLY ONLY GOTHAM & SLOAN ARE INCLUDED IN savedAlphabet, SO YOU
+% MUST CHOOSE EITHER SLOAN OR GOTHAM IF YOU ARE READING YOUR ALPHABET
+% FROM DISK. THIS IS RECOMMENDED TO AVOID COMPATIBILITY ISSUES OF FONT
+% RENDERING.
+o.readAlphabetFromDisk=1; % 1 makes the program more portable.
+if 0
+   % For normal adults we use the restricted standard Sloan alphabet
+   % (excluding C, which has been shown to be too similar to O).
+   o.targetFont='Sloan';
+   o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
+   o.borderLetter='X';
+   o.validKeys={'D','H','K','N','O','R','S','V','Z'};
+else
+   % THESE FONTS ARE DESIGNED TO BE LEGIBLE AT SMALL SIZES.
+   % o.targetFont='ClearviewText';
+   % o.targetFont='Gotham Cond SSm Book';
+   o.targetFont='Gotham Cond SSm Medium';
+   % o.targetFont='Gotham Cond SSm Bold';
+   % o.targetFont='Retina Micro';
+   % o.targetFont='Sticks'; % Available only if o.readAlphabetFromDisk=1
+   o.alphabet='123456789';
+   o.borderLetter='0';
+   o.validKeys = {'1!','2@','3#','4$','5%','6^','7&','8*','9('};
+end
 
 % Set up for interleaved testing of size and spacing thresholds. In the
 % first run we'll use repeated targets. In the second run we'll use single
@@ -162,18 +168,19 @@ o(2).thresholdParameter='size';
 % letters.
 oRepeated=CriticalSpacing(o); % dual targets, repeated indefinitely
 
-% SECOND RUN (measures two thresholds, interleaved)
-% We retain the observer name obtained during the first run for use in the
-% second run.
-o(1).thresholdParameter='size';
-o(1).repeatedTargets=0;
-o(2).repeatedTargets=0;
-o(1).observer=oRepeated(1).observer;
-o(2).observer=oRepeated(2).observer;
-o=o(1);
-% Test two conditions interleaved: 'spacing' and 'size', with single
-% target.
-%oSingle=CriticalSpacing(o); % one target
-
+if 0
+   % SECOND RUN (measures two thresholds, interleaved)
+   % We retain the observer name obtained during the first run for use in the
+   % second run.
+   o(1).thresholdParameter='size';
+   o(1).repeatedTargets=0;
+   o(2).repeatedTargets=0;
+   o(1).observer=oRepeated(1).observer;
+   o(2).observer=oRepeated(2).observer;
+   o=o(1);
+   % Test two conditions interleaved: 'spacing' and 'size', with single
+   % target.
+   oSingle=CriticalSpacing(o); % one target
+end
 % Results are printed in the command window and saved in the "data" folder
 % within the folder that contains the CriticalSpacing.m program.
