12f6340712235836a77851ada632bd020cceb62f
denispelli <denis.pelli@nyu.edu>
2016-01-02 01:00:38 +0100

    cosmetic

--- lib/GetKeypress.m
+++ lib/GetKeypress.m
@@ -10,52 +10,43 @@ function response = GetKeypress(enableKeys, deviceIndex, simulateGetChar)
 % Renamed "GetKeypress" by Denis Pelli, November 2015.
 
 isDebug = 0;
-
 if nargin >= 1
-  % make sure enableKeys are cell strings
-  oldEnableKeys=RestrictKeysForKbCheck(enableKeys);
-  if isDebug; disp('Enabled keys list is:');disp(enableKeys);end
+   % make sure enableKeys are cell strings
+   oldEnableKeys=RestrictKeysForKbCheck(enableKeys);
+   if isDebug; disp('Enabled keys list is:');disp(enableKeys);end
 end
 if nargin<2
-  % All keyboards and keypads.
-  deviceIndex=-3;
+   % Accept input from all keyboards and keypads.
+   deviceIndex=-3;
 end
 if nargin<3
-  % simulate the bahavior of GetChar() -- only return the character response
-  % discarding the second character when two are captured
-  simulateGetChar = 1;
+   % Simulate the behavior of GetChar(): Return only the initial character,
+   % discarding the second, when two are captured.
+   simulateGetChar = 1;
 end
 KbName('UnifyKeyNames'); % make sure this is set
-
-
 while KbCheck; end
 response=0;
 % use modern Kb* functions
-[secs, keyCode] = KbStrokeWait(deviceIndex); % we only need keyCode
+[~,keyCode] = KbStrokeWait(deviceIndex);
 response = KbName(keyCode);
-if isDebug;fprintf('You pressed ==>%s<==\n', response);end
-
+if isDebug;fprintf('You pressed ?%s?.\n',response);end
 if simulateGetChar %KbStrokeWait/KbWait ignores shift
-  % KbName returns 2 characters, e.g. '0)', when you press a number
-  % key on the main keyboard. So when KbName returns two characters,
-  % we return the first and discard the second.  Thus we do not
-  % distinguish between a number key on a number pad and a number key
-  % on the main keyboard.
-  if length(response)==2
-    response=response(1);
-  end
-  if streq(response,'space'); response=' '; end
-  if ismember(response, {'period', '.>', '.'}); response = '.'; end
-  if isDebug;fprintf('We recorded ==>%s<==\n', response);end
-  
-  
+   % KbName returns 2 characters, e.g. '0)', when you press a number key on
+   % the main keyboard. So when KbName returns two characters, we return
+   % the first and discard the second.  Thus we do not distinguish between
+   % a number key on a number pad and a number key on the main keyboard.
+   if length(response)==2
+      response=response(1);
+   end
+   if streq(response,'space'); response=' '; end
+   if ismember(response, {'period', '.>', '.'}); response = '.'; end
+   if isDebug;fprintf('We recorded ?%s?.\n', response);end
 else
-  % return all that is captured
-  if isDebug;fprintf('We recorded ==>%s<==\n', response);end
+   % return all that is captured
+   if isDebug;fprintf('We recorded ?%s?.\n', response);end
 end
-
-
 if nargin >= 1
-  RestrictKeysForKbCheck(oldEnableKeys);
+   RestrictKeysForKbCheck(oldEnableKeys);
 end
 end
