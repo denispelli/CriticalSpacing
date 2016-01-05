cd961a167b57b253cf54874a1c22b2f12d71080c
denispelli <denis.pelli@nyu.edu>
2016-01-05 12:29:06 +0100

    cosmetic
    cleaned up the comments

--- lib/GetKeypress.m
+++ lib/GetKeypress.m
@@ -1,27 +1,28 @@
-function response = GetKeypress(enableKeys, deviceIndex, simulateGetChar)
+function response = GetKeypress(enableKeys,deviceIndex,simulateGetChar)
 % Wait for keypress, and return the lowercase character, e.g. 'a' or '4',
-% or key name, e.g. 'left_shift'. We do not distinguish between pressing a
-% number key on the main or separate numeric keyboard; we just return the
-% one-digit number as a character. simulateGetChar ensures this behavoir.
-% If simulateGetChar is set to false, then full key codes are returned
-% instead, e.g. 'a' or '4$'.
+% or key name, e.g. 'left_shift'. If simulateGetChar is false, then full
+% key codes are returned, e.g. 'a' or '4$'. If simulateGetChar is true then
+% "response" does not distinguish between pressing a number key on the main
+% or separate numeric keyboard; we just return the one-digit number as a
+% character.
 %
 % Originally called "checkResponse" written by Hormet Yiltiz, October 2015.
 % Renamed "GetKeypress" by Denis Pelli, November 2015.
 
 isDebug = 0;
 if nargin >= 1
-   % make sure enableKeys are cell strings
+   % Make sure enableKeys are cell strings
    oldEnableKeys=RestrictKeysForKbCheck(enableKeys);
-   if isDebug; disp('Enabled keys list is:');disp(enableKeys);end
+   if isDebug; disp('Enabled keys list is:'); disp(enableKeys); end
 end
 if nargin<2
    % Accept input from all keyboards and keypads.
    deviceIndex=-3;
 end
 if nargin<3
-   % Simulate the behavior of GetChar(): Return only the initial character,
-   % discarding the second, when two are captured.
+   % By default, simulate the behavior of GetChar(): Return only the
+   % initial character, discarding the second, when KbName returns two,
+   % e.g. for the '1!' key.
    simulateGetChar = 1;
 end
 KbName('UnifyKeyNames'); % make sure this is set
@@ -31,7 +32,7 @@ response=0;
 [~,keyCode] = KbStrokeWait(deviceIndex);
 response = KbName(keyCode);
 if isDebug;fprintf('You pressed ?%s?.\n',response);end
-if simulateGetChar %KbStrokeWait/KbWait ignores shift
+if simulateGetChar % KbStrokeWait/KbWait ignores shift
    % KbName returns 2 characters, e.g. '0)', when you press a number key on
    % the main keyboard. So when KbName returns two characters, we return
    % the first and discard the second.  Thus we do not distinguish between
@@ -41,11 +42,10 @@ if simulateGetChar %KbStrokeWait/KbWait ignores shift
    end
    if streq(response,'space'); response=' '; end
    if ismember(response, {'period', '.>', '.'}); response = '.'; end
-   if isDebug;fprintf('We recorded ?%s?.\n', response);end
 else
    % return all that is captured
-   if isDebug;fprintf('We recorded ?%s?.\n', response);end
 end
+if isDebug;fprintf('We recorded ?%s?.\n', response); end
 if nargin >= 1
    RestrictKeysForKbCheck(oldEnableKeys);
 end
