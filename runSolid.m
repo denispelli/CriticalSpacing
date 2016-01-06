217b8f9442331f3828138882ed9a9c54cd433d53
denispelli <denis.pelli@nyu.edu>
2016-01-06 01:43:14 -0500

    increase viewing distance

--- runSolid.m
+++ runSolid.m
@@ -16,7 +16,7 @@ else
 end
 o.setTargetHeightOverWidth=0;
 o.trials=20; % Number of trials (i.e. responses) for the threshold estimate.
-o.viewingDistanceCm=200;
+o.viewingDistanceCm=400;
 o.flipScreenHorizontally=0; % Set to 1 when using a mirror.
 o.useSpeech=1;
 o.beepPositiveFeedback=1;
