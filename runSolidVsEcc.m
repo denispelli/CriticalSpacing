7f4f1d3b692540623f5177e8e80b9566151f834a
denispelli <denis.pelli@nyu.edu>
2016-01-08 22:57:40 -0500

    run scripts for Pelli font

--- runSolidVsEcc.m
+++ runSolidVsEcc.m
@@ -6,7 +6,7 @@
 % thresholds distributed over 5 runs. This will produce a beautiful graph
 % for the paper.
 clear o
-o.isChild=1;
+o.isChild=0;
 
 if o(1).isChild
    % FOR CHILDREN
@@ -16,16 +16,15 @@ if o(1).isChild
    o.speakEachLetter=1;
 else
    % FOR ADULTS
-   o.showProgressBar=1;
+   o.showProgressBar=0;
    o.fractionEasyTrials=0; % Add 20% extra easy trials. 0 for none.
    o.speakEncouragement=0; % 1 to say "good," "very good," or "nice" after every trial.
    o.speakEachLetter=1;
 end
-o(1).isChild=1;
 
 o.setTargetHeightOverWidth=0;
 o.trials=20; % Number of trials (i.e. responses) for the threshold estimate.
-o.viewingDistanceCm=400;
+o.viewingDistanceCm=40;
 o.flipScreenHorizontally=0; % Set to 1 when using a mirror.
 o.useSpeech=1;
 o.beepPositiveFeedback=1;
@@ -36,12 +35,12 @@ o.usePurring=0;
 o.measureThresholdVertically=0;
 o.observer=''; % Ask for name at beginning of run, or
 % o.observer='Shivam'; % enter observer name here.
-o.readAlphabetFromDisk=1; % 1 makes the program more portable.
+o.readAlphabetFromDisk=0; % 1 makes the program more portable.
 o.usePurring=0; % Play purring sound while awaiting user response.
 % o.radialOrTangential='tangential'; % vertically arranged flankers for single target
 o.radialOrTangential='radial'; % horizontally arranged flankers for single target
 o.negativeFeedback=0;
-o.fixationCrossDeg=0;
+o.fixationCrossDeg=1;
 o.durationSec=inf; % duration of display of target and flankers
 o.measureBeta=0;
 o.textFont='Calibri';
@@ -62,7 +61,7 @@ o.minimumTargetPix=8; % Make sure the letters are well rendered.
 o.displayAlphabet=0; 
 o.showLineOfLetters=0;
 o.showBounds=0;
-o.frameTheTarget=0; 
+o.frameTheTarget=1; 
 o.printSizeAndSpacing=0;
 o.speakSizeAndSpacing=0;
 o.useFractionOfScreen=0; 
@@ -71,7 +70,7 @@ o.useFractionOfScreen=0;
 % first run we'll use repeated targets. In the second run we'll use single
 % targets.
 
-if ~o(1).isChild
+if 0
    % FIRST RUN (measures two thresholds, interleaved) Sloan size
    o=o(1);
    o.fixedSpacingOverSize=1.5; % Requests size proportional to spacing.
@@ -85,50 +84,18 @@ if ~o(1).isChild
    o=CriticalSpacing(o); % dual targets, repeated indefinitely
 end
 
-% SECOND RUN (measures 3 thresholds, interleaved) repeated Solid spacing
 o=o(1);
-o.targetFont='Solid';
+o.targetFont='Pelli';
 o.alphabet='123456789'; 
 o.borderLetter='0';
 o.validKeys = {'1!','2@','3#','4$','5%','6^','7&','8*','9('};
-o.repeatedTargets=1;
+o.repeatedTargets=0;
 o.thresholdParameter='spacing';
-o(2)=o(1); % Copy the condition
-o(3)=o(1); % Copy the condition
-o(1).fixedSpacingOverSize=1.2; % Requests size proportional to spacing.
-o(2).fixedSpacingOverSize=1.5; % Requests size proportional to spacing.
-o(3).fixedSpacingOverSize=1.8; % Requests size proportional to spacing.
-o=CriticalSpacing(o); % dual targets, repeated indefinitely
-
-if ~o(1).isChild
-   % THIRD RUN (measures 3 thresholds, interleaved) repeated Solid size
-   o=o(1);
-   o.repeatedTargets=1;
-   o.thresholdParameter='size';
-   o(2)=o(1); % Copy the condition
-   o(3)=o(1); % Copy the condition
-   o(1).fixedSpacingOverSize=1.2; % Requests size proportional to spacing.
-   o(2).fixedSpacingOverSize=1.5; % Requests size proportional to spacing.
-   o(3).fixedSpacingOverSize=1.8; % Requests size proportional to spacing.
-   o=CriticalSpacing(o); % dual targets, repeated indefinitely
-   
-   % 4TH RUN (measures 3 thresholds, interleaved) single Solid spacing
-   o=o(1);
-   o.repeatedTargets=0;
-   o.thresholdParameter='spacing';
-   o(2)=o(1); % Copy the condition
-   o(3)=o(1); % Copy the condition
-   o(1).fixedSpacingOverSize=1.2; % Requests size proportional to spacing.
-   o(2).fixedSpacingOverSize=1.5; % Requests size proportional to spacing.
-   o(3).fixedSpacingOverSize=1.8; % Requests size proportional to spacing.
-   o=CriticalSpacing(o); % dual targets, repeated indefinitely
-   
-   % 5TH RUN (measures 1 threshold) single Solid size
-   o=o(1);
-   o.repeatedTargets=0;
-   o.thresholdParameter='size';
-   o=CriticalSpacing(o); % dual targets, repeated indefinitely
-end
-
+o.fixedSpacingOverSize=1.5; % Requests size proportional to spacing.
+o.durationSec=0.2;
+o.eccentricityDeg=5;
+% o(2)=o(1); % Copy the condition
+% o(3)=o(1); % Copy the condition
+o=CriticalSpacing(o); 
 % Results are printed in the command window and saved in the "data" folder
 % within the folder that contains the CriticalSpacing.m program.
