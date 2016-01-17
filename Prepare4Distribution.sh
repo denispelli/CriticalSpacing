77c534b745ec8d4e79549b80c698dd3fb0334ba5
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2016-01-17 03:38:24 -0500

    run this .sh script to zip software with Git independent VERSION tag

---
+++ Prepare4Distribution.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# export our software to share with co-workers
+# Only to be used by developers!
+#
+# Hormet Yiltiz, 2016
+#
+
+# this script should be put in ./lib/
+#cd ..
+
+
+echo "Updating version file..."
+git log | head -n 3 > VERSION
+
+echo "Zipping latest software version..."
+#git archive -9 -o "${PWD##*/}"-`git rev-parse --abbrev-ref HEAD`-`date "+%Y-%m-%d"`-`git log --pretty=format:"%h" -n 1`.zip HEAD
+
+FNAME="${PWD##*/}"-`git rev-parse --abbrev-ref HEAD`-`date "+%Y-%m-%d"`-`git log --pretty=format:"%h" -n 1`.zip
+git archive -9 -o $FNAME HEAD
+
+echo ""
+zip add $FNAME VERSION
+echo ""
+
+echo "$FNAME generated!"
