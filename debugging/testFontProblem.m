edd534b9b36f2960e7bd0a9ba734ff65f3c0cee0
denispelli <denis.pelli@nyu.edu>
2016-01-28 23:32:42 -0500

    test program, can't reproduce problem

---
+++ debugging/testFontProblem.m
@@ -0,0 +1,10 @@
+% trying to replicate problem on michelle fogarty's mac.
+% no success.
+Screen('Preference', 'SkipSyncTests', 1);
+textFont='Trebuchet MS';
+[window,r]=Screen('OpenWindow',0,255,[0 0 100 100]);
+Screen('TextSize',window,14);
+Screen('TextFont',window,textFont,0);
+Screen('TextFont',window)
+boundsRect=Screen('TextBounds',window,'hello');
+sca
