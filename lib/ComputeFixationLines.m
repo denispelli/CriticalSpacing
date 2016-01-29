4c79665ec420d209c9d5e1f0a897f34449c24532
denispelli <denis.pelli@nyu.edu>
2016-01-28 23:44:20 -0500

    update for non-square letters

--- lib/ComputeFixationLines.m
+++ lib/ComputeFixationLines.m
@@ -22,8 +22,11 @@ function fixationLines=ComputeFixationLines(fix)
 %                                       % target eccentricity. We also
 %                                       % blank a radius proportional to
 %                                       % target radius.
-% fix.blankingRadiusReTargetHeight=1.5; % Make blanking radius 1.5 times
-%                                       % target height.
+% fix.blankingRadiusReTargetHeight=1.5; % Make vertical blanking radius 1.5
+%                                       % times target height.
+% fix.blankingRadiusReTargetWidth=1.5;  % Make horizontal blanking radius 
+%                                       % 1.5 times target width.
+% fix.targetHeightOverWidth=1;          % 1 for Sloan. 5 for Pelli font.
 % fix.targetHeightPix=targetHeightPix;  % Blanking radius is proportional
 %                                       % to specified target height.
 % fix.targetCross=1;                    % Draw vertical line indicating
@@ -44,10 +47,18 @@ end
 if ~isfield(fix,'fixationCrossBlankedNearTarget')
     fix.fixationCrossBlankedNearTarget=1; % Default is yes.
 end
-if ~isfield(fix,'blankingRadiusReTargetRadius')
+if ~isfield(fix,'blankingRadiusReTargetHeight')
     fix.blankingRadiusReTargetHeight=1.5; % Blank a radius of 1.5 times target height.
 end
-blankingRadiusPix=fix.blankingRadiusReTargetHeight*fix.targetHeightPix;
+if ~isfield(fix,'blankingRadiusReTargetWidth')
+    fix.blankingRadiusReTargetWidth=1.5; % Blank a radius of 1.5 times target width.
+end
+if ~isfield(fix,'targetHeightOverWidth') || ~isfinite(fix.targetHeightOverWidth)
+   warning('fix.targetHeightOverWidth is undefined. Assuming it is 1.');
+   fix.targetHeightOverWidth=1;
+end
+blankingHeightPix=fix.blankingRadiusReTargetHeight*fix.targetHeightPix;
+blankingWidthPix=fix.blankingRadiusReTargetWidth*fix.targetHeightPix/fix.targetHeightOverWidth;
 
 % We initially use abs(eccentricity) and assume fixation is at (0,0). At
 % the end, we adjust for polarity of eccentricity and the actual location
@@ -64,8 +75,8 @@ if 0>=r(2) && 0<=r(4) % Fixation is on screen.
     lineStart=max(lineStart,r(1)); % clip to fix.clipRect
     lineEnd=min(lineEnd,r(3)); % clip to fix.clipRect
     if fix.fixationCrossBlankedNearTarget
-        blankStart=min(abs(fix.eccentricityPix)*(1-fix.bouma),abs(fix.eccentricityPix)-blankingRadiusPix);
-        blankEnd=max(abs(fix.eccentricityPix)*(1+fix.bouma),abs(fix.eccentricityPix)+blankingRadiusPix);
+        blankStart=min(abs(fix.eccentricityPix)*(1-fix.bouma),abs(fix.eccentricityPix)-blankingWidthPix);
+        blankEnd=max(abs(fix.eccentricityPix)*(1+fix.bouma),abs(fix.eccentricityPix)+blankingWidthPix);
     else
         blankStart=lineStart-1;
         blankEnd=blankStart;
@@ -104,13 +115,13 @@ if 0>=r(1) && 0<=r(3) % Fixation is on screen.
     lineStart=max(lineStart,r(2)); % clip to fix.clipRect
     lineEnd=min(lineEnd,r(4)); % clip to fix.clipRect
     fixationLinesV=[];
-    if ~fix.fixationCrossBlankedNearTarget || abs(fix.eccentricityPix)>blankingRadiusPix
+    if ~fix.fixationCrossBlankedNearTarget || abs(fix.eccentricityPix)>blankingHeightPix
         % no blanking of line
         fixationLinesV(1:2,1:2)=[0 0;lineStart lineEnd];
-    elseif lineStart<-blankingRadiusPix
+    elseif lineStart<-blankingHeightPix
         % blank breaks the line
-        fixationLinesV(1:2,1:2)=[0 0; lineStart -blankingRadiusPix];
-        fixationLinesV(1:2,3:4)=[0 0; blankingRadiusPix lineEnd];
+        fixationLinesV(1:2,1:2)=[0 0; lineStart -blankingHeightPix];
+        fixationLinesV(1:2,3:4)=[0 0; blankingHeightPix lineEnd];
     else
         % whole line is blanked
         fixationLinesV=[0 0;0 0];
@@ -130,10 +141,10 @@ if fix.targetCross && fix.eccentricityPix>=r(1) && fix.eccentricityPix<=r(3)
     if ~fix.fixationCrossBlankedNearTarget
         % no blanking of line
         fixationLinesV(1:2,1:2)=[0 0;lineStart lineEnd];
-    elseif lineStart<-blankingRadiusPix
+    elseif lineStart<-blankingHeightPix
         % blank breaks the line
-        fixationLinesV(1:2,1:2)=[0 0; lineStart -blankingRadiusPix];
-        fixationLinesV(1:2,3:4)=[0 0; blankingRadiusPix lineEnd];
+        fixationLinesV(1:2,1:2)=[0 0; lineStart -blankingHeightPix];
+        fixationLinesV(1:2,3:4)=[0 0; blankingHeightPix lineEnd];
     else
         % whole line is blanked
         fixationLinesV=[0 0;0 0];
