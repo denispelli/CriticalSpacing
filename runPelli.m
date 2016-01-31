08a9371cf0df9a2d4d06b2deb65118c6117dab10
denispelli <denis.pelli@nyu.edu>
2016-01-31 02:48:20 -0500

    tidied up

--- runPelli.m
+++ runPelli.m
@@ -1,6 +1,60 @@
 % MATLAB script to run CriticalSpacing.m
 % Copyright 2015,2016, Denis G. Pelli, denis.pelli@nyu.edu
 clear o
+
+% PROCEDURE
+o.trials=20; % Number of trials (i.e. responses) for the threshold estimate.
+o.readAlphabetFromDisk=1; % 1 makes the program more portable.
+o.viewingDistanceCm=400;
+o.flipScreenHorizontally=0; % Set to 1 when using a mirror.
+o.minimumTargetPix=6; % Minimum viewing distance depends soley on this & pixPerCm.
+o.observer=''; % Ask for name at beginning of run, or
+% o.observer='Shivam'; % enter observer name here.
+o.measureBeta=0;
+o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing.
+o.measureThresholdVertically=0;
+o.measureBeta=0;
+
+% SOUND
+o.useSpeech=1;
+o.beepPositiveFeedback=1;
+o.beepNegativeFeedback=0;
+o.usePurring=0; % Play purring sound while awaiting user response.
+
+% TASK
+o.task='identify';
+o.trials=3; % Number of trials (i.e. responses) for the threshold estimate.
+
+% VISUAL STIMULUS
+o.durationSec=inf; % duration of display of target and flankers
+o.repeatedTargets=1;
+o.fourFlankers=1;
+o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing.
+o.setTargetHeightOverWidth=0;
+o.measureThresholdVertically=0;
+% o.radialOrTangential='tangential'; % vertically arranged flankers for single target
+o.radialOrTangential='radial'; % horizontally arranged flankers for single target
+o.textFont='Calibri';
+% o.targetFont='Sloan';
+% o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
+% o.borderLetter='X';
+o.targetFont='Pelli';
+o.alphabet='123456789'; 
+o.borderLetter='$';
+
+% FIXATION
+o.fixationCrossDeg=0;
+o.fixationLocation='center';
+
+% DEBUGGING AIDS
+o.showAlphabet=0; 
+o.showLineOfLetters=0;
+o.showBounds=0;
+o.frameTheTarget=0; 
+o.printSizeAndSpacing=0;
+o.speakSizeAndSpacing=0;
+o.useFractionOfScreen=0; 
+
 if 0
    % FOR CHILDREN
    o.showProgressBar=1;
@@ -14,63 +68,8 @@ else
    o.speakEncouragement=0; % 1 to say "good," "very good," or "nice" after every trial.
    o.speakEachLetter=1;
 end
-o.setTargetHeightOverWidth=0;
-o.trials=20; % Number of trials (i.e. responses) for the threshold estimate.
-o.viewingDistanceCm=400;
-o.flipScreenHorizontally=0; % Set to 1 when using a mirror.
-o.useSpeech=1;
-o.beepPositiveFeedback=1;
-o.beepNegativeFeedback=0;
-o.usePurring=0;
-o.minimumTargetPix=6; % Minimum viewing distance depends soley on this & pixPerCm.
-
-% You don't need to change any of these parameters.
-o.measureThresholdVertically=0;
-o.observer=''; % Ask for name at beginning of run, or
-% o.observer='Shivam'; % enter observer name here.
-o.readAlphabetFromDisk=1; % 1 makes the program more portable.
-o.usePurring=0; % Play purring sound while awaiting user response.
-% o.radialOrTangential='tangential'; % vertically arranged flankers for single target
-o.radialOrTangential='radial'; % horizontally arranged flankers for single target
-o.negativeFeedback=0;
-o.fixationCrossDeg=0;
-o.durationSec=inf; % duration of display of target and flankers
-o.measureBeta=0;
-o.textFont='Calibri';
-o.fixationLocation='center';
-o.task='identify';
-% o.targetFont='Sloan';
-% o.targetFont='ClearviewText';
-% o.targetFont='Gotham Cond SSm XLight';
-% o.targetFont='Gotham Cond SSm Light';
-% o.targetFont='Gotham Cond SSm Medium';
-% o.targetFont='Gotham Cond SSm Book';
-% o.targetFont='Gotham Cond SSm Bold';
-% o.targetFont='Gotham Cond SSm Black';
-% o.targetFont='Arouet';
-% o.targetFont='Retina Micro';
-% o.targetFont='Calibri';
-o.targetFont='Pelli';
-o.alphabet='123456789'; 
-o.borderLetter='$';
-o.validKeys = {'1!','2@','3#','4$','5%','6^','7&','8*','9('};
-
-% DEBUGGING AIDS
-o.displayAlphabet=0; 
-o.showLineOfLetters=0;
-o.showBounds=0;
-o.frameTheTarget=0; 
-o.printSizeAndSpacing=0;
-o.speakSizeAndSpacing=0;
-o.useFractionOfScreen=0; 
-
-o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing.
 
 % FIRST RUN (measures two thresholds, interleaved)
-o.repeatedTargets=0;
-o.fourFlankers=1;
-o.thresholdParameter='spacing';
-o.durationSec=1;
 o(2)=o(1); % Copy the condition
 o=CriticalSpacing(o); 
 
