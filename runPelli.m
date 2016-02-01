92279e5cd8ca6dc31260ba768da2f66d8fe7070b
denispelli <denis.pelli@nyu.edu>
2016-02-01 00:25:03 -0500

    cosmetic

--- runPelli.m
+++ runPelli.m
@@ -10,12 +10,12 @@ o.flipScreenHorizontally=0; % Set to 1 when using a mirror.
 o.minimumTargetPix=6; % Minimum viewing distance depends soley on this & pixPerCm.
 o.observer=''; % Ask for name at beginning of run, or
 % o.observer='Shivam'; % enter observer name here.
-o.measureBeta=0;
-o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing.
 o.measureThresholdVertically=0;
-o.measureBeta=0;
+o.textFont='Calibri';
+o.showProgressBar=1;
 
 % SOUND
+o.speakEachLetter=1;
 o.useSpeech=1;
 o.beepPositiveFeedback=1;
 o.beepNegativeFeedback=0;
@@ -23,7 +23,6 @@ o.usePurring=0; % Play purring sound while awaiting user response.
 
 % TASK
 o.task='identify';
-o.trials=3; % Number of trials (i.e. responses) for the threshold estimate.
 
 % VISUAL STIMULUS
 o.durationSec=inf; % duration of display of target and flankers
@@ -31,10 +30,8 @@ o.repeatedTargets=1;
 o.fourFlankers=1;
 o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing.
 o.setTargetHeightOverWidth=0;
-o.measureThresholdVertically=0;
 % o.radialOrTangential='tangential'; % vertically arranged flankers for single target
 o.radialOrTangential='radial'; % horizontally arranged flankers for single target
-o.textFont='Calibri';
 % o.targetFont='Sloan';
 % o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
 % o.borderLetter='X';
@@ -45,6 +42,9 @@ o.borderLetter='$';
 % FIXATION
 o.fixationCrossDeg=0;
 o.fixationLocation='center';
+
+% QUEST
+o.measureBeta=0;
 
 % DEBUGGING AIDS
 o.showAlphabet=0; 
@@ -57,27 +57,23 @@ o.useFractionOfScreen=0;
 
 if 0
    % FOR CHILDREN
-   o.showProgressBar=1;
    o.fractionEasyTrials=0.2; % 0.2 adds 20% easy trials. 0 adds none.
    o.speakEncouragement=1; % 1 to say "good," "very good," or "nice" after every trial.
-   o.speakEachLetter=1;
 else
    % FOR ADULTS
-   o.showProgressBar=1;
    o.fractionEasyTrials=0; % Add 20% extra easy trials. 0 for none.
    o.speakEncouragement=0; % 1 to say "good," "very good," or "nice" after every trial.
-   o.speakEachLetter=1;
 end
 
-% FIRST RUN (measures two thresholds, interleaved)
+% FIRST RUN (measure two thresholds, interleaved)
 o(2)=o(1); % Copy the condition
 o=CriticalSpacing(o); 
 
-% SECOND RUN (measures two thresholds, interleaved)
+% SECOND RUN (measure two thresholds, interleaved)
 o=o(1);
 o(1).repeatedTargets=1;
 o(2)=o(1);
-%o=CriticalSpacing(o); 
+o=CriticalSpacing(o); 
 
 % Results are printed in the command window and saved in the "data" folder
 % within the folder that contains the CriticalSpacing.m program.
