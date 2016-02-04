93d009a7cc8f9497268a0e1b584d2a690732cc94
denispelli <denis.pelli@nyu.edu>
2016-02-04 02:26:09 -0500

    updated scripts

--- runVisualAcuity.m
+++ runVisualAcuity.m
@@ -1,48 +1,43 @@
 % MATLAB script to run CriticalSpacing.m
-% Copyright 2015, Denis G. Pelli, denis.pelli@nyu.edu
+% Copyright 2015,2016, Denis G. Pelli, denis.pelli@nyu.edu
 clear o
-if 0
-   % FOR CHILDREN
-   o.showProgressBar=1;
-   o.fractionEasyTrials=0.2; % 0.2 adds 20% easy trials. 0 adds none.
-   o.speakEncouragement=1; % 1 to say "good," "very good," or "nice" after every trial.
-   o.speakEachLetter=1;
-else
-   % FOR ADULTS
-   o.showProgressBar=1;
-   o.fractionEasyTrials=0; % Add 20% extra easy trials. 0 for none.
-   o.speakEncouragement=0; % 1 to say "good," "very good," or "nice" after every trial.
-   o.speakEachLetter=1;
-end
-o.setTargetHeightOverWidth=0;
+
+% PROCEDURE
+o.viewingDistanceCm=400; % Default for runtime question.
+o.experimenter=''; % Put name here to skip the runtime question.
+o.observer=''; % Put name here to skip the runtime question.
 o.trials=20; % Number of trials (i.e. responses) for the threshold estimate.
-o.viewingDistanceCm=400;
+o.readAlphabetFromDisk=1; % 1 makes the program more portable.
 o.flipScreenHorizontally=0; % Set to 1 when using a mirror.
+o.minimumTargetPix=6; % Minimum viewing distance depends soley on this & pixPerCm.
+o.measureThresholdVertically=0;
+o.textFont='Arial';
+o.showProgressBar=1;
+o.task='identify';
+
+% SOUND
+o.speakEachLetter=1;
 o.useSpeech=1;
 o.beepPositiveFeedback=1;
 o.beepNegativeFeedback=0;
-o.usePurring=0;
-
-% You don't need to change any of these parameters.
-o.measureThresholdVertically=0;
-o.observer=''; % Ask for name at beginning of run, or
-% o.observer='Shivam'; % enter observer name here.
-o.readAlphabetFromDisk=0; % 1 makes the program more portable.
 o.usePurring=0; % Play purring sound while awaiting user response.
+
+% VISUAL STIMULUS
+o.durationSec=inf; % duration of display of target and flankers
+o.repeatedTargets=1;
+o.fourFlankers=1;
+o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing.
+o.setTargetHeightOverWidth=0;
 % o.radialOrTangential='tangential'; % vertically arranged flankers for single target
 o.radialOrTangential='radial'; % horizontally arranged flankers for single target
-o.negativeFeedback=0;
-o.fixationCrossDeg=0;
-o.durationSec=inf; % duration of display of target and flankers
-o.measureBeta=0;
-o.textFont='Calibri';
-o.fixationLocation='center';
-o.task='identify';
-o.minimumTargetPix=8; % Make sure the letters are well rendered.
+
+% TARGET FONT
 o.targetFont='Sloan';
 o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
 o.borderLetter='X';
-o.validKeys={'D','H','K','N','O','R','S','V','Z'};
+% o.targetFont='Pelli';
+% o.alphabet='123456789'; 
+% o.borderLetter='$';
 % o.targetFont='ClearviewText';
 % o.targetFont='Gotham Cond SSm XLight';
 % o.targetFont='Gotham Cond SSm Light';
@@ -53,13 +48,16 @@ o.validKeys={'D','H','K','N','O','R','S','V','Z'};
 % o.targetFont='Arouet';
 % o.targetFont='Pelli';
 % o.targetFont='Retina Micro';
-% o.targetFont='Calibri';
-% o.alphabet='123456789'; 
-% o.borderLetter='0';
-% o.validKeys = {'1!','2@','3#','4$','5%','6^','7&','8*','9('};
-% 
+
+% FIXATION
+o.fixationCrossDeg=0;
+o.fixationLocation='center';
+
+% QUEST threshold estimation
+o.measureBeta=0;
+
 % DEBUGGING AIDS
-o.displayAlphabet=0; 
+o.showAlphabet=0; 
 o.showLineOfLetters=0;
 o.showBounds=0;
 o.frameTheTarget=0; 
@@ -67,40 +65,29 @@ o.printSizeAndSpacing=0;
 o.speakSizeAndSpacing=0;
 o.useFractionOfScreen=0; 
 
+% FOR CHILDREN
+% o.fractionEasyTrials=0.2; % 0.2 adds 20% easy trials. 0 adds none.
+% o.speakEncouragement=1; % 1 to say "good," "very good," or "nice" after every trial.
+
 % Set up for interleaved testing of size and spacing thresholds. In the
 % first run we'll use repeated targets. In the second run we'll use single
 % targets.
 
-o.fixedSpacingOverSize=1.5; % Requests size proportional to spacing.
-
-% FIRST RUN (measures two thresholds, interleaved)
+% RUN measure thresholds for size
+o.durationSec=0.2; % duration of display of target and flankers
 o.repeatedTargets=0;
 o.fourFlankers=1;
 o.thresholdParameter='size';
-% o(2)=o(1); % Copy the condition
-% o(2).thresholdParameter='size';
-% o(1).fixedSpacingOverSize=1.5; % Requests size proportional to spacing.
-% o(2).fixedSpacingOverSize=1.2; % Requests size proportional to spacing.
-% Test two conditions interleaved: 'spacing' and 'size', with repeated
-% letters.
-if 1
-   o.eccentricityDeg=10;
-   o.durationSec=1;
-   o.useFractionOfScreen=0.3; 
-
-end
+o.fixationCrossDeg=3;
+o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing.
+o.eccentricityDeg=0;
+o.fixationLocation='center';
 o=CriticalSpacing(o); % dual targets, repeated indefinitely
 
-if 0
-   % SECOND RUN (measures two thresholds, interleaved)
-% We retain the observer name obtained during the first run for use in the
-% second run.
-o(1).observer=oRepeated(1).observer;
-o(2).observer=oRepeated(1).observer;
-o(1).repeatedTargets=0;
-o(2).repeatedTargets=0;
-%o=CriticalSpacing(o); % one target
-end
+% RUN measure thresholds for size
+o.eccentricityDeg=10;
+o.fixationLocation='left';
+o=CriticalSpacing(o); % dual targets, repeated indefinitely
 
-% Results are printed in the command window and saved in the "data" folder
-% within the folder that contains the CriticalSpacing.m program.
+% Results are printed in MATLAB's Command Window and saved in the
+% CriticalSpacing/data/ folder.
