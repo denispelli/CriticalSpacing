db02f8181f0960a8a59221bc0ec8fb74ae7ff486
denispelli <denis.pelli@nyu.edu>
2016-02-22 04:12:24 -0500

    enhanced to compare TextBounds vs Screen TextBounds

--- debugging/TestTextBounds.m
+++ debugging/TestTextBounds.m
@@ -1,25 +1,29 @@
-textSize=48;
+font='Pelli'; string='8';
+% font='Arial'; string='O';
+textSize=100;
+Screen('Preference','SkipSyncTests',1);
+Screen('Preference','Verbosity',0); % Mute Psychtoolbox's INFOs and WARNINGs
 w=Screen('OpenWindow',0,255);
-Screen(w,'TextFont','Arial');
+Screen(w,'TextFont',font);
 Screen(w,'TextSize',textSize);
 wScratch=Screen('OpenOffscreenWindow',w,[],[0 0 2*textSize*length(string) 2*textSize]);
-Screen(wScratch,'TextFont','Arial');
+Screen(wScratch,'TextFont',font);
 Screen(wScratch,'TextSize',textSize);
 x=50;
 y=50+textSize;
 for yPositionIsBaseline=0:1
-    if yPositionIsBaseline
-        string='Origin at baseline. ';
-    else
-        string='Origin at upper left.';
-    end
-    t=GetSecs;
-    bounds=TextBounds(wScratch,string,yPositionIsBaseline)
-    fprintf('TextBounds took %.3f seconds.\n',GetSecs-t);
-    [newX,newY]=Screen('DrawText',w,string,x,y,0,255,yPositionIsBaseline);
-    Screen('FrameRect',w,0,InsetRect(OffsetRect(bounds,x,y),-1,-1));
-    x=newX;
-    y=newY;
+   t=GetSecs;
+   textBounds=TextBounds(wScratch,string,yPositionIsBaseline);
+   fprintf('Width %4.0f, Height %4.0f, TextBounds took %.2f ms. Green.\n',RectWidth(textBounds),RectHeight(textBounds),1000*(GetSecs-t));
+   t=GetSecs;
+   [screenTextBounds,screenOffsetTextBounds]=Screen('TextBounds',wScratch,string,0,0,yPositionIsBaseline);
+   fprintf(2,'Width %4.0f, Height %4.0f, Screen TextBounds took %.2f ms. Red.\n',RectWidth(screenOffsetTextBounds),RectHeight(screenOffsetTextBounds),1000*(GetSecs-t));
+   [newX,newY]=Screen('DrawText',w,string,x,y,0,255,yPositionIsBaseline);
+   Screen('FrameRect',w,[0 255 0],InsetRect(OffsetRect(textBounds,x,y),-1,-1));
+   %     Screen('FrameRect',w,[0 0 255],InsetRect(OffsetRect(screenTextBounds,x,y),-1,-1));
+   Screen('FrameRect',w,[255 0 0],InsetRect(OffsetRect(screenOffsetTextBounds,x,y),-1,-1));
+   x=newX;
+   y=newY;
 end
 Screen('Close',wScratch);
 Screen('Flip',w);
