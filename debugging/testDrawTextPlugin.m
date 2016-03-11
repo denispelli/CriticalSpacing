3a60deb59b13e4864fe6246844f7f242c1d1be43
denispelli <denis.pelli@nyu.edu>
2016-03-11 05:30:39 -0500

    check drawText plugin

--- debugging/testDrawTextPlugin.m
+++ debugging/testDrawTextPlugin.m
@@ -1,9 +1,11 @@
+% Checks (and reports) whether the standard DrawText Plugin loads.
+% Denis Pelli 2016
 Screen('Preference','SuppressAllWarnings',1);
 Screen('Preference','VisualDebugLevel',0);
 Screen('Preference','Verbosity',0); % Mute Psychtoolbox's INFOs and WARNINGs
-Screen('Preference', 'SkipSyncTests', 1);
-oldRenderer=Screen('Preference','TextRenderer',1) % Request FGTL DrawText plugin.
-renderer=Screen('Preference','TextRenderer',1) % Request FGTL DrawText plugin.
+Screen('Preference','SkipSyncTests',1);
+Screen('Preference','TextRenderer',1); % Request FGTL DrawText plugin.
+fprintf('%d: renderer %d\n',MFileLineNr,Screen('Preference','TextRenderer'));
 window=Screen('OpenWindow',0);
 Screen('TextFont',window,'Arial');
 fprintf('%d: renderer %d\n',MFileLineNr,Screen('Preference','TextRenderer'));
