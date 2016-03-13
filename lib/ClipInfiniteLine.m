7c3a486a9c1d6abae740f5200114d0b89aa3107e
denispelli <denis.pelli@nyu.edu>
2016-03-13 17:44:22 -0400

    polish

--- lib/ClipInfiniteLine.m
+++ lib/ClipInfiniteLine.m
@@ -1,14 +1,17 @@
 function [x,y]=ClipInfiniteLine(x,y,r)
 %[x,y]=ClipInfiniteLine(x,y,r);
-% Clips a line by a rect, and returns the new line segment (possibly zero
-% length) or nothing (empty rects). The line segment argument defines an
-% infinite line going through the segment, extended indefinitely in both
-% directions. The line segment is (x(1),y(1)) to (x(2),y(2)). Returns nans
-% if you provide a zero-length or otherwise undefined line.
+% Clips an infinite line by a rect, and returns the new line segment
+% (possibly zero length) or nothing (empty rects). The line segment
+% argument defines an infinite line colinear with the segment. The line
+% segment is (x(1),y(1)) to (x(2),y(2)). Direction (from point 1 to point
+% 2) is preserved. Returns nans if you provide a zero-length or otherwise
+% ambiguous line segment that does not specify an infinite line.
 % 2016 denis.pelli@nyu.edu
 
 % Make sure the two points define a line.
-if any([isnan(x) isnan(y)]) || (x(1)==x(2) && y(1)==y(2))
+if (x(1)==x(2) && y(1)==y(2)) || ...
+      (any(~isfinite(x)) && y(1)~=y(2)) || ...
+      (any(~isfinite(y)) && x(1)~=x(2))
    x=[nan nan];
    y=[nan nan];
    return
@@ -40,8 +43,8 @@ if y(1)==y(2)
    return
 end
 % Now we know the line isn't horizontal or vertical, so we can always
-% compute x from y, and visa versa. Use the x-range of the rect to select a
-% segment of the line.
+% compute x from y, and visa versa. First use the x-range of the rect to
+% clip the line down to a line segment.
 slope=(y(2)-y(1))/(x(2)-x(1));
 xNew=[r(1) r(3)];
 yNew=slope*(xNew-x(1))+y(1);
@@ -53,7 +56,7 @@ if all(y<r(2)) || all(y>r(4))
    y=[];
    return
 end
-% Use the y-range of the rect to select a segment of the line.
+% Now use the y-range of the rect to clip the line segment.
 for i=1:2
    yNew(i)=min(y(i),r(4));
    yNew(i)=max(yNew(i),r(2));
