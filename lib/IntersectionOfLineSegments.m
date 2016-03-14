984b5d177b5fac077383d5ffb4fb27082bc8dc0c
denispelli <denis.pelli@nyu.edu>
2016-03-14 04:39:51 -0400

    added jnd to tolerate small numerical error.

--- lib/IntersectionOfLineSegments.m
+++ lib/IntersectionOfLineSegments.m
@@ -2,12 +2,15 @@ function [x,y]=IntersectionOfLineSegments(lineA,lineB)
 % Each column is the x and y coordinates of a point. Each line segment
 % has two points, one per column.
 % 2016 denis.pelli@nyu.edu
+% March 14, 2016, Add jnd of 1e-10 to tolerate numerical error, which was
+% 1e-14 in the case I encountered.
 [x,y]=IntersectionOfInfiniteLines(lineA,lineB);
 if isnan(x) || isnan(y)
    return
 end
-isXInLineSegment=@(x,line)(x>=line(1,1) && x<=line(1,2)) || (x<=line(1,1) && x>=line(1,2));
-isYInLineSegment=@(y,line)(y>=line(2,1) && y<=line(2,2)) || (y<=line(2,1) && y>=line(2,2));
+jnd=1e-10;
+isXInLineSegment=@(x,line)(x>=line(1,1)-jnd && x<=line(1,2))+jnd || (x<=line(1,1)+jnd && x>=line(1,2)-jnd);
+isYInLineSegment=@(y,line)(y>=line(2,1)-jnd && y<=line(2,2)+jnd) || (y<=line(2,1)+jnd && y>=line(2,2)-jnd);
 if isXInLineSegment(x,lineA) && isYInLineSegment(y,lineA) && isXInLineSegment(x,lineB) && isYInLineSegment(y,lineB) 
    return
 end
