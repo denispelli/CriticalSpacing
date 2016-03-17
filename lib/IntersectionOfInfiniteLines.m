d8b1b0eff8276fc22f83b83dd03e54189b28b548
denispelli <denis.pelli@nyu.edu>
2016-03-17 01:59:57 -0400

    polish comments

--- lib/IntersectionOfInfiniteLines.m
+++ lib/IntersectionOfInfiniteLines.m
@@ -1,16 +1,16 @@
 function [x,y]=IntersectionOfInfiniteLines(lineA,lineB)
-% Each "line" is a two-row vector containing the x and y coordinates of the line
-% segments: Pairs of consecutive columns define (x,y) positions of the starts and
-% ends of line segments. 
-% The provided line segments define (infinite) lines. If the lines intersect, we
-% return a point of intersection. If the infinite lines are identical we
-% return a point on one of the supplied line segments.
-% If the lines do not intersect we return NAN for both x and y.
-% It is an error for either line segment to have zero length.
+% Each "line" is a two-row vector containing the x and y coordinates of the
+% line segments: Pairs of consecutive columns define (x,y) positions of the
+% starts and ends of line segments. The provided line segments define
+% (infinite) lines. If the lines intersect, we return a point of
+% intersection. If the infinite lines are identical we return a point on
+% one of the supplied line segments. If the lines do not intersect we
+% return NAN for both x and y. It is an error for either line segment to
+% have zero length.
 % 2016 by denis.pelli@nyu.edu
-bA=(lineA(2,1)-lineA(2,2))/(lineA(1,1)-lineA(1,2));
+bA=(lineA(2,1)-lineA(2,2))/(lineA(1,1)-lineA(1,2)); % dy/dx
 aA=lineA(2,1)-bA*lineA(1,1);
-bB=(lineB(2,1)-lineB(2,2))/(lineB(1,1)-lineB(1,2));
+bB=(lineB(2,1)-lineB(2,2))/(lineB(1,1)-lineB(1,2)); % dy/dx
 aB=lineB(2,1)-bB*lineB(1,1);
 if isnan(bA) || isnan(bB)
    error('Both lines must have greater than zero length.');
