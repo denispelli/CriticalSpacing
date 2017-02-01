91f14577ca1b24fac26ffe8075d6f56636c5c8e6
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2017-02-01 13:52:03 -0500

    Platform-independently detect alpha numeric characters

--- lib/CreateLetterTextures.m
+++ lib/CreateLetterTextures.m
@@ -185,8 +185,9 @@ end
 
 function u = urlencoding(s)
   u = '';
+
   for k = 1:length(s),
-    if isalnum(s(k))
+    if ~isempty(regexp(s(k), '[a-zA-Z0-9]', 'once'))
       u(end+1) = s(k);
     else
       u=[u,'%',dec2hex(s(k)+0)];
