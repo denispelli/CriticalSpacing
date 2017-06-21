18f2a9ea2317105f668073a706b60a9f1e1787ab
denispelli <denis.pelli@nyu.edu>
2017-06-21 04:00:42 -0400

    new function

---
+++ lib/ClipLine2.m
@@ -0,0 +1,11 @@
+function [newXY1,newXY2] = ClipLine2(xy1,xy2,r)
+% [xy1,xy2,] = ClipLine2(xy1,xy2,r);
+% Given a line segment specifying a line and a rect, returns the clipped
+% line. If there's nothing left the output arguments are NaN.
+dxy=xy2-xy1;
+dr=r(1:2)-r(3:4);
+scalar=1e6*sqrt(sum(dr.^2))/sqrt(sum(dxy.^2));
+% We extend the provided line segment to be 1e6 times bigger than the
+% diagonal length of the rect, in both directions.
+% That's good enough for my application.
+[newXY1, newXY2] = ClipLineSegment2(xy1+scalar*dxy, xy1-scalar*dxy, r);
