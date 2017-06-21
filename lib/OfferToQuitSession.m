df9d3edc71144492837f525903d5d41080d2fa4b
denispelli <denis.pelli@nyu.edu>
2017-06-21 15:11:19 -0400

    polish
    now only offer RETURN to proceed, and ESCAPE or GRAVE ACCENT to quit.

--- lib/OfferToQuitSession.m
+++ lib/OfferToQuitSession.m
@@ -4,23 +4,22 @@ if oo(1).speakEachLetter && oo(1).useSpeech
    Speak('Escape');
 end
 escapeKeyCode=KbName('ESCAPE');
-spaceKeyCode=KbName('space');
+% spaceKeyCode=KbName('space');
 returnKeyCode=KbName('return');
 graveAccentKeyCode=KbName('`~');
 escapeChar=char(27);
 graveAccentChar='`';
 Screen('FillRect',window);
 Screen('TextFont',window,oo(1).textFont,0);
-string='Quitting the run. Hit ESCAPE again to quit the whole session. To proceed with the next run, hit SPACE or RETURN.';
+string='Quitting the run. Hit ESCAPE again to quit the whole session. Or hit RETURN to proceed with the next run.';
 black=0;
 white=255;
 DrawFormattedText(window,string,instructionalMargin,instructionalMargin-0.5*oo(1).textSize,black,65,[],[],1.1);
-% Screen('DrawText',window,'Hit ESCAPE to terminate, or SPACE or RETURN to continue:',instructionalMargin,screenRect(4)/2);
 Screen('TextSize',window,round(oo(1).textSize*0.35));
 Screen('DrawText',window,double('Crowding and Acuity Test, Copyright 2016, 2017, Denis Pelli. All rights reserved.'),instructionalMargin,screenRect(4)-0.5*instructionalMargin,black,white,1);
 Screen('TextSize',window,oo(1).textSize);
 Screen('Flip',window);
-answer=GetKeypress([returnKeyCode spaceKeyCode escapeKeyCode graveAccentKeyCode],oo(1).deviceIndex);
+answer=GetKeypress([returnKeyCode escapeKeyCode graveAccentKeyCode],oo(1).deviceIndex);
 quitSession=ismember(answer,[escapeChar,graveAccentChar]);
 if oo(1).useSpeech
    if quitSession
