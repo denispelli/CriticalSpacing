08d5b9acf07cadb412dd9c7f2bd27f43990b1bb1
denispelli <denis.pelli@nyu.edu>
2017-06-21 15:08:54 -0400

    cope with infinite lines

--- lib/ClipLineSegment2.m
+++ lib/ClipLineSegment2.m
@@ -4,8 +4,8 @@ function [newXY1,newXY2]=ClipLineSegment2(xy1,xy2,r)
 % Liang-Barsky Algorithm for line clipping. See
 % http://en.wikipedia.org/wiki/Liang%E2%80%93Barsky_algorithm
 %
-% Take a 2-D line from xy1 to xy2, and clip to rectangle r with corners at
-% v1, v2. If nothing remains the output arguments are NaN.
+% Take a 2-D line from xy1 to xy2, and clip to fit in rectangle r. If
+% nothing remains, the output arguments are NaN.
 %
 % Inputs must be vectors of length 2. Outputs are vectors of length 2. If
 % no outputs are specified, a plot is made.
@@ -30,21 +30,36 @@ x_max = max(v1(1),v2(1));
 y_min = min(v1(2),v2(2));
 y_max = max(v1(2),v2(2));
 
+% Quick hack to trim infinite line that is horizontal or
+% vertical. Without this hack we 
+if any(~isfinite([x0 x1])) && y0==y1
+   x0=min(x0,x_max);
+   x0=max(x0,x_min);
+   x1=min(x1,x_max);
+   x1=max(x1,x_min);
+end
+if any(~isfinite([y0 y1])) && x0==x1
+   y0=min(y0,y_max);
+   y0=max(y0,y_min);
+   y1=min(y1,y_max);
+   y1=max(y1,y_min);
+end
+
 newXY1 = nan(size(xy1));
 newXY2 = nan(size(xy2));
 
-dx = x1 - x0;
-dy = y1 - y0;
+dx = x1-x0;
+dy = y1-y0;
 
 p = [ -dx;
    dx;
    -dy;
    dy ];
 
-q = [ x0 - x_min;   % negative => left of window
-   x_max - x0;   % negative => right of window
-   y0 - y_min;   % negative => below window
-   y_max - y0;   % negative => above window
+q = [ x0-x_min;   % negative => left of window
+   x_max-x0;   % negative => right of window
+   y0-y_min;   % negative => below window
+   y_max-y0;   % negative => above window
    ];
 
 % Test if line isn't visible.
@@ -96,14 +111,16 @@ if 0
    % This demo produces a pretty plot.
    figure;
    
-   ClippedLine([-1,-1],[3,2],[0 0 1 1]);
-   ClippedLine([0.5,0.3],[1.3,0.7],[0 0 1 1]);
-   ClippedLine([-0.5,1.2],[1.3,0.7],[0 0 1 1]);
-   ClippedLine([-0.5,0.7],[0.3,0.7],[0 0 1 1]);
-   ClippedLine([0.2,1.3],[0.1,-0.4],[0 0 1 1]);
+   ClipLineSegment2([-1,-1],[3,2],[0 0 1 1]);
+   ClipLineSegment2([0.5,0.3],[1.3,0.7],[0 0 1 1]);
+   ClipLineSegment2([-0.5,1.2],[1.3,0.7],[0 0 1 1]);
+   ClipLineSegment2([-0.5,0.7],[0.3,0.7],[0 0 1 1]);
+   ClipLineSegment2([0.2,1.3],[0.1,-0.4],[0 0 1 1]);
    
-   [newXY1,newXY2] =ClippedLine([0,-.04],[0,-0.04],[0 0 1 1]);
-   ClippedLine([0.2,.5],[0.8,.5],[0 0 1 1]);
+   [newXY1,newXY2] =ClipLineSegment2([0,-.04],[0,-0.04],[0 0 1 1]);
+   ClipLineSegment2([0.2,.5],[0.8,.5],[0 0 1 1]);
+   ClipLineSegment2([-inf .3],[inf,.3],[0 0 1 1]);
+   %    ClipLineSegment2([-100 .5],[100,.5],[0 0 1 1]);
 end
 end
 
