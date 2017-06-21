142b3dd96a381ffb130e9cd5b95927a555b1e834
denispelli <denis.pelli@nyu.edu>
2017-06-21 15:10:23 -0400

    improved
    now call a more-reliable subroutine to do the work

--- lib/ClipLines.m
+++ lib/ClipLines.m
@@ -1,11 +1,16 @@
 function clippedLines=ClipLines(lines,r)
-%lines=ClipLines(lines,r);
-% Clips each line segment by a rect, and returns the new line segment.
-% Omits the line when there is no intersection.
-% 2016 denis.pelli@nyu.edu
+% lines=ClipLines(lines,r);
+% Clips each line segment by a rect, and returns the new line segments.
+% Omits lines entirely outside the rect.
+% 2016, 2017 denis.pelli@nyu.edu
 for i=1:2:size(lines,2)
    line=lines(:,i:i+1);
-   [x,y,xx,yy]=ClipLine(line(1,1),line(2,1),line(1,2),line(2,2),r);
+%    [x,y,xx,yy]=ClipLine(line(1,1),line(2,1),line(1,2),line(2,2),r);
+   [xy1,xy2]=ClipLineSegment2(line(:,1),line(:,2),r);
+   x=xy1(1);
+   y=xy1(2);
+   xx=xy2(1);
+   yy=xy2(2);
    if any(isnan([x xx y yy]))
       assert(all(isnan([x xx y yy])))
    end
