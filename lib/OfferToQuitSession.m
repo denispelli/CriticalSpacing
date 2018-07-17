19f6b5f5c5baa2ef04ea783c2998c76d476fadf2
denispelli <denis.pelli@nyu.edu>
2018-07-17 03:32:27 -0400

    add 2018

--- lib/OfferToQuitSession.m
+++ lib/OfferToQuitSession.m
@@ -16,7 +16,7 @@ black=0;
 white=255;
 DrawFormattedText(window,string,instructionalMargin,instructionalMargin-0.5*oo(1).textSize,black,65,[],[],1.1);
 Screen('TextSize',window,round(oo(1).textSize*0.35));
-Screen('DrawText',window,double('Crowding and Acuity Test, Copyright 2016, 2017, Denis Pelli. All rights reserved.'),instructionalMargin,screenRect(4)-0.5*instructionalMargin,black,white,1);
+Screen('DrawText',window,double('Crowding and Acuity Test, Copyright 2016, 2017, 2018, Denis Pelli. All rights reserved.'),instructionalMargin,screenRect(4)-0.5*instructionalMargin,black,white,1);
 Screen('TextSize',window,oo(1).textSize);
 Screen('Flip',window);
 answer=GetKeypress([returnKeyCode escapeKeyCode graveAccentKeyCode],oo(1).deviceIndex);
