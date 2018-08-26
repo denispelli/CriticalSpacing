82cfb2e6ff16da39dcd75af19b48f43de651442a
denispelli <denis.pelli@nyu.edu>
2018-08-26 16:50:10 -0400

    to test o.maxLines parameter

--- testMaxLines.m
+++ testMaxLines.m
@@ -2,18 +2,22 @@
 % Test the new o.maxLines feature. o.maxLines can have any integer value of
 % 3 or more, up to inf.
 clear o oo
-o.targetFont='Sloan';
-o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
-o.borderLetter='X';
+% o.targetFont='Sloan';
+% o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
+% o.borderLetter='X';
+o.targetFont='Pelli';
+o.alphabet='123456789'; 
+o.borderLetter='$';
 o.thresholdParameter='spacing'; % 'spacing' or 'size'
 o.isolatedTarget=false;
 o.conditionName='crowding';
 o.fixationLineWeightDeg=0.04;
 o.fixationCrossDeg=3; % 0, 3, and inf are typical values.
-o.eccentricityXYDeg=[0 1];
-o.flankingDirection='tangential';
+o.eccentricityXYDeg=[0 0];
+o.flankingDirection='horizontal';
 o.thresholdParameter='spacing'; % 'spacing' or 'size'
 o.viewingDistanceCm=50;
+o.fixedSpacingOverSize=1.4; % Requests size proportional to spacing, horizontally and vertically.
 o.condition=1;
 o.trials=30;
 o.practicePresentations=0;
@@ -23,7 +27,8 @@ o.durationSec=2; % duration of display of target and flankers
 % o.printSizeAndSpacing=true;
 % o.useFractionOfScreen=0.5;
 o.repeatedTargets=true;
-o.maxLines=3;
+o.maxFixationErrorXYDeg=[3 3]; % Repeat targets enough to cope with fixation errors up to this size.
+o.maxLines=1; % Must be 1,3,4,...inf
 % o.maxLines=inf;
 o=CriticalSpacing(o);
 
