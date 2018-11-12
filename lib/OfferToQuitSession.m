5017a1b9e3fece03d07ff75c2ad47a153d5a4f12
denispelli <denis.pelli@nyu.edu>
2018-11-12 06:06:55 -0500

    change “run” to “block”

--- lib/OfferToQuitSession.m
+++ lib/OfferToQuitSession.m
@@ -11,7 +11,7 @@ escapeChar=char(27);
 graveAccentChar='`';
 Screen('FillRect',window);
 Screen('TextFont',window,oo(1).textFont,0);
-string='Quitting the run. Hit ESCAPE again to quit the whole session. Or hit RETURN to proceed with the next run.';
+string='Quitting the block. Hit ESCAPE again to quit the whole session. Or hit RETURN to proceed with the next block.';
 black=0;
 white=255;
 DrawFormattedText(window,string,instructionalMargin,instructionalMargin-0.5*oo(1).textSize,black,65,[],[],1.1);
@@ -25,7 +25,7 @@ if oo(1).useSpeech
    if quitSession
       Speak('Escape. Done.');
    else
-      Speak('Proceeding to next run.');
+      Speak('Proceeding to next block.');
    end
 end
 Screen('FillRect',window);
