871b1790ddc9e6eb979e1fcc90fa8ba8d8de004b
denispelli <denis.pelli@nyu.edu>
2018-11-27 03:22:26 -0500

    o.simulateObserver
    update comments
    increase ecc from 5 to 15 deg.
    don’t repeat
    o.simulateObserver

--- runCrowdingSurvey.m
+++ runCrowdingSurvey.m
@@ -1,30 +1,18 @@
 % runCrowdingSurvey.m
 % MATLAB script to run CriticalSpacing.m
-% Copyright 2015,2016,2017 Denis G. Pelli, denis.pelli@nyu.edu
+% Copyright 2018 Denis G. Pelli, denis.pelli@nyu.edu
 % 
-% I estimate that runCrowdingSurvey will take 30 minutes to complete. It
-% measures 12 thresholds. It tests two locations: (-5,0) deg and (+5,0)
+% I estimate that runCrowdingSurvey will take 15 minutes to complete. It
+% measures 6 thresholds. It tests two locations: (-15,0) deg and (+15,0)
 % deg. At each location it measure acuity, plus crowding radially and
-% tangentially. It does everything twice. That adds up to 12 thresholds.
+% tangentially. 
 % 
 % The script specifies "Darshan" as experimenter. You can change that in
-% the script below if necessary. The word "run" has been replaced by
-% "block". On the first block the program will ask the observer's name. On
-% subsequent blocks it will remember the observer's name.
+% the script below if necessary. On the first block the program will ask
+% the observer's name. On subsequent blocks it will remember the observer's
+% name.
 % 
-% PLEASE USE BINOCULAR VIEWING, USING BOTH EYES ALL THE TIMES.
-% 
-% The script specifies a viewing distance of 75 cm. That's a good distance.
-% However, that might be awkward for an observer with short arms. In that
-% case you have two choices. Either: 1. provide a remote keyboard. We have
-% lots, mostly stored above Qihan's desk. One of the options, when the
-% program says "Welcome to CriticalSpacing", is to  type "k", which
-% provokes it to recreate the list of keyboards, to notice that you've
-% provided a wireless keyboard. 2. The other option is to change the
-% viewing distance. At that same screen you can type in the new viewing
-% distance, e.g. 60 cm. It will remember for the rest of the testing of
-% that observer. You can make the viewing distance anything between 60 and
-% 150 cm.
+% PLEASE USE BINOCULAR VIEWING, USING BOTH EYES AT ALL TIMES.
 % 
 % IT IS VERY IMPORTANT TO USE A METER STICK OR TAPE MEASURE TO ACTUALLY
 % MEASURE THE VIEWING DISTANCE AND ENSURE THAT THE OBSERVER'S EYE IS
@@ -38,11 +26,14 @@
 % 646-258-7524
 
 % Crowding distance at 2 location x 2 orientation. Acuity at 2 locations.
-% * (4 thresholds). Horizontal meridian: ±5 deg X orientations (0, 90 deg)
+% * (4 thresholds). Horizontal meridian: ±15 deg X orientations (0, 90 deg)
 % * (2 thresholds). Acuity at ±5 deg ecc.
-% Repeat all of it. Total of 12 thresholds.
+% Total of 6 thresholds.
 
 clear o oo ooo
+% SIMULATE OBSERVER TO TEST THRESHOLD ESTIMATION
+% oo(oi).simulateObserver=true;
+% oo(oi).simulatedLogThreshold=0;
 o.targetFont='Sloan';
 o.alphabet='DHKNORSVZ'; % Sloan alphabet, excluding C
 o.borderLetter='X';
