20291373beea50c991acb0019be7de5c40607045
denispelli <denis.pelli@nyu.edu>
2018-11-27 04:29:40 -0500

    cosmetic clean up

--- runCrowdingSurvey.m
+++ runCrowdingSurvey.m
@@ -4,33 +4,29 @@
 % 
 % I estimate that runCrowdingSurvey will take 15 minutes to complete. It
 % measures 6 thresholds. It tests two locations: (-15,0) deg and (+15,0)
-% deg. At each location it measure acuity, plus crowding radially and
-% tangentially. 
+% deg. At each location it measures acuity, plus crowding radially and
+% tangentially.
 % 
 % The script specifies "Darshan" as experimenter. You can change that in
 % the script below if necessary. On the first block the program will ask
 % the observer's name. On subsequent blocks it will remember the observer's
 % name.
-% 
+%
 % PLEASE USE BINOCULAR VIEWING, USING BOTH EYES AT ALL TIMES.
 % 
-% IT IS VERY IMPORTANT TO USE A METER STICK OR TAPE MEASURE TO ACTUALLY
-% MEASURE THE VIEWING DISTANCE AND ENSURE THAT THE OBSERVER'S EYE IS
-% ACTUALLY AT THE DISTANCE THAT THE PROGRAM THINKS IT IS. PLEASE ENCOURAGE
-% THE OBSERVER TO MAINTAIN THE SAME VIEWING DISTANCE FOR THE WHOLE
-% EXPERIMENT. From this perspective, a few cm variation matters less from a
-% larger viewing distance. So err on the high side when selecting viewing
-% distance.
+% IMPORTANT: Use a meter stick or tape measure to actually measure the
+% viewing distance and ensure that the observer's eye is actually at the
+% distance that the program thinks it is. Please encourage the observer to
+% maintain the same viewing distance for the whole experiment. From this
+% perspective, a few cm variation matters less from a larger viewing
+% distance. So err on the high side when selecting viewing distance.
 %
 % denis.pelli@nyu.edu November 12, 2018
 % 646-258-7524
 
-% Crowding distance at 2 location x 2 orientation. Acuity at 2 locations.
-% * (4 thresholds). Horizontal meridian: ±15 deg X orientations (0, 90 deg)
-% * (2 thresholds). Acuity at ±5 deg ecc.
-% Total of 6 thresholds.
 
 clear o oo ooo
+% oo(oi).useFractionOfScreen=0.5;
 % SIMULATE OBSERVER TO TEST THRESHOLD ESTIMATION
 % o.simulateObserver=true;
 % o.simulatedLogThreshold=0;
@@ -40,26 +36,21 @@ o.borderLetter='X';
 o.experiment='CrowdingSurvey';
 ooo={};
 for rep=1
-    for crowding=[1 0]
-        if crowding
-            o.conditionName='crowdingDistance';
-            o.thresholdParameter='spacing';
-            for radial=0:1
-                if radial
-                    o.flankingDirection='radial';
-                else
-                    o.flankingDirection='tangential';
-                end
-                ooo{end+1}=o;
-            end
-        else % acuity
-            o.conditionName='acuity';
-            o.thresholdParameter='size';
-            o.flankingDirection='radial'; % Ignored
-            ooo{end+1}=o;
+    o.conditionName='crowdingDistance';
+    o.thresholdParameter='spacing';
+    for radial=0:1
+        if radial
+            o.flankingDirection='radial';
+        else
+            o.flankingDirection='tangential';
         end
-        
+        ooo{end+1}=o;
     end
+    
+    o.conditionName='acuity';
+    o.thresholdParameter='size';
+    o.flankingDirection=''; % Ignored
+    ooo{end+1}=o;
 end
 % Test each condition at two symmetric locations, randomly interleaved.
 for i=1:length(ooo)
@@ -69,28 +60,27 @@ for i=1:length(ooo)
     o.nearPointXYInUnitSquare=[0.5 0.5];
     o.eccentricityXYDeg=[-15 0];
     radialDeg=sqrt(sum(o.eccentricityXYDeg.^2));
-    o.viewingDistanceCm=max(30,min(400,round(9/tand(radialDeg))));
+%     o.viewingDistanceCm=max(30,min(400,round(9/tand(radialDeg))));
     o.viewingDistanceCm=40;
     oo=o;
     o.eccentricityXYDeg= -o.eccentricityXYDeg;
     oo(2)=o;
     ooo{i}=oo;
 end
+
+% PRINT A TABLE OF ALL THE CONDITIONS
 oo=[];
 for i=1:length(ooo)
-    if isempty(oo)
-        oo=ooo{i};
-    else
-        oo(end+1:end+2)=ooo{i};
-    end
+    oo=[oo ooo{i}];
 end
 t=struct2table(oo);
-t % Print the conditions in the Command Window.
-% return
+disp(t); % Print the conditions in the Command Window.
+return
+
+% RUN THE CONDITIONS, ONE BLOCK AT A TIME.
 for i=1:length(ooo)
     oo=ooo{i};
     for oi=1:length(oo)
-%         oo(oi).useFractionOfScreen=0.5;
         if i==1
             oo(oi).experimenter='Darshan';
             oo(oi).observer='';
@@ -103,7 +93,6 @@ for i=1:length(ooo)
         oo(oi).fixationLineWeightDeg=0.1;
         oo(oi).fixationCrossDeg=1; % 0, 3, and inf are typical values.
         oo(oi).trials=30;
-        oo(oi).practicePresentations=0;
         oo(oi).durationSec=0.1; % duration of display of target and flankers
         oo(oi).repeatedTargets=0;
     end
@@ -115,5 +104,5 @@ for i=1:length(ooo)
     if any([oo.quitSession])
         break
     end
-    old=oo(1); % Allow reuse of settings.
+    old=oo(1); % Allow reuse of settings from previous block.
 end
