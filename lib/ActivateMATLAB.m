b3afba754688ed656576d0771f5dee820673c7eb
denispelli <denis.pelli@nyu.edu>
2018-11-26 23:11:51 -0500

    add comment

--- lib/ActivateMATLAB.m
+++ lib/ActivateMATLAB.m
@@ -1,5 +1,8 @@
 function ActivateMATLAB
 % This applescript command provokes a screen refresh (by selecting MATLAB).
+%
+% Unfortunately it takes 3 seconds to run. I'm not sure why. It appears
+% that AppleScript is always slow.
 % denis.pelli@nyu.edu, June 18, 2015
 
 status=system('osascript -e ''tell application "MATLAB" to activate''');
