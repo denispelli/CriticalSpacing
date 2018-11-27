41e2d1efe7716069cc66cc5d80a87c0b14efbca3
denispelli <denis.pelli@nyu.edu>
2018-11-27 05:37:44 -0500

    documentation

--- lib/CloseWindowsAndCleanup.m
+++ lib/CloseWindowsAndCleanup.m
@@ -1,6 +1,18 @@
 function CloseWindowsAndCleanup()
 % Close any windows opened by the Psychtoolbox Screen command, 
-% re-enable the keyboard, show the cursor, and restore the cluts.
+% re-enable the keyboard, show the cursor, and restore the cluts. This
+% function is similar to "sca", but much quicker if no window is open.
+%
+% It can be frustrating to have your program terminate, possibly due to an
+% error, while a psychtoolbox window obscures your view of the MATLAB
+% Command Window. You can avoid that problem by planning ahead. Call
+% onCleanup at the beginning of your main program to request a clean up
+% whenever your program terminates, even by error or control-c.
+%
+% cleanup=onCleanup(@() CloseWindowsAndCleanup);
+%
+% denis.pelli@nyu.edu, November 27, 2018
+
 if ~isempty(Screen('Windows'))
     % Screen CloseAll is very slow, so we call it only if we need to.
     Screen('CloseAll');
@@ -14,7 +26,4 @@ ShowCursor;
 RestoreCluts;
 end % function CloseWindowsAndCleanup()
 
-% Call onCleanup at the beginning of your main program to request a clean
-% up whenever your program terminates, even by error or control-c.
-% cleanup=onCleanup(@() CloseWindowsAndCleanup);
 
