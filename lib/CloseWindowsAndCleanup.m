5f90b6b5bf117f6eb3d06e4bcc80ea0e4f7de5bf
denispelli <denis.pelli@nyu.edu>
2018-11-27 05:47:03 -0500

    cosmetic

--- lib/CloseWindowsAndCleanup.m
+++ lib/CloseWindowsAndCleanup.m
@@ -1,7 +1,7 @@
 function CloseWindowsAndCleanup()
-% Close any windows opened by the Psychtoolbox Screen command, 
-% re-enable the keyboard, show the cursor, and restore the cluts. This
-% function is similar to "sca", but much quicker if no window is open.
+% Closes any windows opened by the Psychtoolbox Screen command, re-enables
+% the keyboard, shows the cursor, and restores the video color lookup
+% tables (cluts). This function is similar to "sca".
 %
 % It can be frustrating to have your program terminate, possibly due to an
 % error, while a psychtoolbox window obscures your view of the MATLAB
@@ -14,12 +14,12 @@ function CloseWindowsAndCleanup()
 % denis.pelli@nyu.edu, November 27, 2018
 
 if ~isempty(Screen('Windows'))
-    Screen('CloseAll');
+    Screen('CloseAll'); % May take many seconds.
     if ismac
-        AutoBrightness(0,1);
+        AutoBrightness(0,1); % May take many seconds.
     end
 end
-% These three are quick, about 0.1 s.
+% These three are quick.
 ListenChar;
 ShowCursor;
 RestoreCluts;
