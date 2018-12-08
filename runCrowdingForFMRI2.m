039f5a4c43953d0504ffb32e5afff896c5b55230
denispelli <denis.pelli@nyu.edu>
2018-12-08 16:46:24 -0500

    lengthen name to o.useFractionOfScreenToDebug

--- runCrowdingForFMRI2.m
+++ runCrowdingForFMRI2.m
@@ -120,7 +120,7 @@ disp(t(:,{'condition','thresholdParameter','eccentricityXYDeg','flankingDirectio
 oOld.observer='';
 for i=1:length(oo)
     o=oo(i);
-    % o.useFractionOfScreen=0.5;
+    % o.useFractionOfScreenToDebug=0.5;
     o.trials=30;
     o.practicePresentations=0;
     o.experimenter='Darshan';
