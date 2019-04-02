47dd0276d7cadbf8b1cd7f98565aef4fffd8458c
denispelli <denis.pelli@nyu.edu>
2019-04-02 10:42:24 -0400

    Skip duplicate blocks.
    Cosmetic update of explanations at the beginning of the file.
    Block files are unique, but might duplicate data with a summary file. (NoiseDiscrimination generates summary files. CriticalSpacing does not yet do so.) By reading both block files and summary files, one might get the same block twice. We skip such duplicates by using the unique dataFilename that each block has.
    Make sure every condition carries the block’s dataFilename, beginSecs, and beginningTime.

--- lib/ReadExperimentData.m
+++ lib/ReadExperimentData.m
@@ -1,39 +1,58 @@
 function [oo,tt]=ReadExperimentData(experiment,vars)
-% [oo,tt]=ReadExperimentData(experiment,vars);
-% Returns all the thresholds contains in all the MAT files in the data
-% folder whose names begin with the string specified in the "experiment"
-% argument. The MAT file may contain a whole experiment ooo{}, or a block
-% oo(), with one element per condition. ooo{} is a cell array of oo. oo is
-% a struct array of o. Each o is a condition with a threshold. The
-% thresholds are extracted from all the MAT files in the data folder whose
-% names begin with the string in "experiment". We add two new fields to
-% each threshold record. "date" is a readable string indicating the date
-% and time of measurement. "missingField" is a cell list of strings of all
-% the fields that were requested in vars, but not available in the
-% threshold record.
+% [oo,tt]=ReadExperimentData(experiment,vars); Returns all the thresholds
+% contains in all the MAT files in the data folder whose names begin with
+% the string specified in the "experiment" argument. The MAT file may
+% contain a whole experiment ooo{}, or a block oo(), with one element per
+% condition. ooo{} is a cell array of oo. oo is a struct array of o. Each o
+% is a condition with a threshold. The thresholds are extracted from all
+% the MAT files in the data folder whose names begin with the string in
+% "experiment". We add two new fields to each threshold record. "date" is a
+% readable string indicating the date and time of measurement.
+% "missingField" is a cell list of strings of all the fields that were
+% requested in vars, but not available in the threshold record.
+%
+% NOTE: Thresholds with fewer than minimumTrials (currently 25) are
+% ignored.
+%
+% The original data files group thresholds into blocks of interleaved
+% conditions. This routine ignores the blocking, returning just a list of
+% conditions (each with a threshold). This offers the convenience of simply
+% indexing all the conditions, which simplifies the analysis programming.
+% The blocking is important for collecting the data, but is not used in my
+% analyses. The blocking is still detectable because every condition within
+% a block has the same dataFilename, beginningTime (1 s precision), and
+% beginSecs (nanosec precision). Getting all the conditions into one struct
+% array is not trivial because every element of a struct array must have
+% the same fields, and we often have unique fields in some conditions. The
+% solution here is to predefine a list of the fields we'll use, and ignore
+% all other fields. Unused fields (in conditions that don't use them) are
+% left empty. Alternatively, to retain all fields, it would be necessary
+% for the whole array to have the union of all the fields in all the
+% conditions. That would have the virtue of not requiring a predefined
+% list.
+%
 % denis.pelli@nyu.edu July 2018
 
 myPath=fileparts(mfilename('fullpath')); % Takes 0.1 s.
 addpath(myPath); % We are in the "lib" folder.
-% lib and data folders are in the same folder.
+% THe lib and data folders are in the same folder.
 dataFolder=fullfile(fileparts(fileparts(mfilename('fullpath'))),'data'); 
 matFiles=dir(fullfile(dataFolder,['run' experiment '*.mat']));
 
-% Each threshold has a unique identifier: o.dataFilename. It is created
+% Each block has a unique identifier: o.dataFilename. It is created
 % just before we start running trials. I think that we could read all the
-% data files, accumulating both individual threshold files, a "o" struct,
+% data files, accumulating both block files, with an "oo" struct,
 % and summary files which contain a whole experiment "ooo", consisting of
-% multiple blocks "oo" each of which contains several thresholds "o".  if
-% we first discard instances with zero trials then we can safely discard
-% duplicates with the same identifier and neither lose data, nor retain any
-% duplicate data.
+% multiple blocks "oo", each of which contains several thresholds "o".  We
+% can safely discard duplicate blocks with the same identifier and neither
+% lose data, nor retain any duplicate data.
 
 % The summary file retains the organization of trials into blocks and
 % experiments. The individual threshold files do not, but they do have
 % "conditionName" "observer" and "experiment" fields that encode the most
 % important aspects of the grouping.
 
-%% READ ALL DATA INTO A BLOCK LIST OF THRESHOLDS "oo".
+%% READ ALL DATA INTO A LIST OF CONDITIONS IN STRUCT ARRAY "oo".
 if nargin<1
     error('You must include the first argument, a string, but it can be an empty string.');
 end
@@ -63,6 +82,7 @@ if nargin<2
         };
 end
 oo=struct([]);
+filenameList={}; % Avoid duplicate data.
 for iFile=1:length(matFiles) % One file per iteration.
     % Accumulate all conditions into one long oo struct array. First we
     % read each file into a temporary ooo{} cell array, each of whose
@@ -90,6 +110,12 @@ for iFile=1:length(matFiles) % One file per iteration.
             % Skip any block lacking a dataFilename (undefined).
             continue
         end
+        if ismember(ooo{block}(1).dataFilename,filenameList)
+            % Skip if we already have this block of data.
+            continue
+        else
+            filenameList{end+1}=ooo{block}(1).dataFilename;
+        end
         for oi=1:length(ooo{block}) % Iterate through conditions within a block.
             if isempty(ooo{block}(oi).dataFilename)
                 % Make sure every condition has this field.
@@ -98,6 +124,10 @@ for iFile=1:length(matFiles) % One file per iteration.
             if isempty(ooo{block}(oi).beginningTime)
                 % Make sure every condition has this field.
                 ooo{block}(oi).beginningTime=ooo{block}(1).beginningTime;
+            end
+            if isempty(ooo{block}(oi).beginSecs)
+                % Make sure every condition has this field.
+                ooo{block}(oi).beginSecs=ooo{block}(1).beginSecs;
             end
             o=ooo{block}(oi); % "o" holds one condition.
             oo(end+1).missingFields={}; % Create new element.
@@ -119,6 +149,8 @@ for iFile=1:length(matFiles) % One file per iteration.
     end
 end
 fprintf('Read %d thresholds from %d files. Now discarding empties and duplicates.\n',length(oo),length(matFiles));
+
+%% CLEAN UP THE LIST, DISCARDING WHAT WE DON'T WANT.
 % We've now gotten all the thresholds into oo. 
 if ~isfield(oo,'trials')
     error('No data');
@@ -146,7 +178,7 @@ for oi=1:length(oo)
     oo(oi).date=sprintf('%04d.%02d.%02d, %02d:%02d:%02.0f',y,m,d,h,mi,s);
 end
 tt=struct2table(oo,'AsArray',true);
-minimumTrials=1; % 25 DGP
+minimumTrials=25; % 25 DGP
 if sum(tt.trials<minimumTrials)>0
     fprintf('\nWARNING: Discarding %d threshold(s) with fewer than %d trials:\n',sum(tt.trials<minimumTrials),minimumTrials);
     disp(tt(tt.trials<minimumTrials,{'date' 'observer' 'thresholdParameter' 'eccentricityXYDeg' 'trials'})) % 'experiment'  'conditionName'
