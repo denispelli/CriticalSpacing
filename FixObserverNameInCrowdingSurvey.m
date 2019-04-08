3e1004cf08920ab1371855d30beb275e727d082b
denispelli <denis.pelli@nyu.edu>
2019-04-08 12:04:13 -0400

    localHostName identifies computer, evolving

--- FixObserverNameInCrowdingSurvey.m
+++ FixObserverNameInCrowdingSurvey.m
@@ -23,20 +23,18 @@
 % 1000 s with an invalid observer (i.e. Darshan), then I copied the valid
 % observer to the new block. I repeated this iteratively, and happily all
 % observers are now valid.
-    
-
 experiment='CrowdingSurvey';
 printFilenames=true;
 makePlotLinear=false;
 myPath=fileparts(mfilename('fullpath')); % Takes 0.1 s.
 addpath(fullfile(myPath,'lib')); % Folder in same directory as this M file.
 dataFolder=fullfile(fileparts(mfilename('fullpath')),'data');
-dataFolder='/Users/denispelli/Dropbox/MATLAB/CriticalSpacing/scratch/';
+% dataFolder='/Users/denispelli/Dropbox/MATLAB/CriticalSpacing/data/';
 cd(dataFolder);
 close all
 %% READ ALL DATA OF EXPERIMENT FILES INTO A LIST OF THRESHOLDS "oo".
 vars={'condition' 'conditionName' 'dataFilename' ... % 'experiment' 
-    'experimenter' 'observer' 'trials' 'thresholdParameter' ...
+    'experimenter' 'observer' 'localHostName' 'trials' 'thresholdParameter' ...
     'eccentricityXYDeg' 'targetDeg' 'spacingDeg' 'flankingDirection'... 
     'viewingDistanceCm' 'durationSec'  ...
     'contrast' 'pixPerCm'  'nearPointXYPix'  'beginningTime' 'beginSecs' 'block' 'blocksDesired' };
@@ -46,36 +44,73 @@ time=datevec(t.beginningTime);
 t.Year=time(:,1);
 t = sortrows(t,'beginningTime');
 t=t(t.Year==2019,:);
-% t=t(ismember(t.experimenter,{'Qihan'}),:);
-t=t(ismember(t.experimenter,{'darshan','Darshan'}),:);
+% t=t(ismember(t.experimenter,{'darshan' 'Darshan'}),:);
 % t=sortrows(t,'block');
-t(:,{'dataFilename' 'Year' 'trials' 'experimenter' 'observer' 'block' 'blocksDesired'  'eccentricityXYDeg' 'conditionName'})
+t(:,{'dataFilename' 'Year' 'trials' 'experimenter' 'observer' 'localHostName' 'block' 'blocksDesired'  'eccentricityXYDeg' 'conditionName'})
 % return
+
+% Compute delta time 's' for each file to the next file.
 [~,ii]=unique(t(:,'dataFilename'));
 t=sortrows(t(ii,:),'beginningTime');
 time=table2array(t(:,'beginningTime'));
+delta=[];
 for i=1:height(t)-1
     delta(i)=etime(datevec(time(i+1)),datevec(time(i)));
 end
 delta(height(t))=nan;
 t(:,'s')=array2table(delta');
-t(:,{'dataFilename' 'experimenter' 'observer' 's' 'block'})
-    return
-% THE FIX: For 2019 files before April 3, when observer is Darshan or Sam
-% Coleman, then set observer to Sam Colman and experimenter to Darshan.
+t(:,{'dataFilename' 'experimenter' 'observer' 'localHostName' 's' 'block'})
+%     return
+    
+% Replace o.observer oldName by newName.
+localHostName='';
+oldName='janet  gu';
+newName='janet gu';
+oldName='';
+newName='ana';
+oldName=' ';
+newName='UNKNOWN';
+oldName='Jessie';
+newName='Jessie Zhanay';
+oldName='Douglas';
+newName='Elizabeth Kurtz';
+localHostName='Pelli-Mac-1';
+localHostName='';
+oldName='';
+newName='UNKNOWN2';
 
-% tt=t(ismember(t.observer,{'Sam Colman'}),:);
-tt=t;
+tt=t(ismember(t.observer,{oldName}),:);
 cd(dataFolder);
 list=table2array(tt(:,'dataFilename'));
-s=table2array(tt(:,'s'));
-% return;
 observer='';
-for f=1:height(t)
-    name=list{f};
-    load([name '.mat'],'oo');
-    assert(streq(oo(1).dataFilename,name));
-    if ~ismember(oo(1).observer,{'Darshan' 'darshan'})
+for f=1:height(tt)
+    filename=list{f};
+    load([filename '.mat'],'oo');
+    assert(streq(oo(1).dataFilename,filename));
+    if ismember(oo(1).observer,{oldName})
+        % If localHostName is not empty, then it must match.
+        if isempty(localHostName) || ismember({oo(1).cal.localHostName},localHostName)
+            % Substitute newName for oldName.
+            oldFilename=filename;
+            filename=strrep(filename,[oldName '.2019'],[newName '.2019']);
+            for oi=1:length(oo)
+                oo(oi).observer=newName;
+                oo(oi).dataFilename=filename;
+            end
+            fprintf('OLD: %s\n',oldFilename);
+            fprintf('NEW: %s\n',filename);
+            fprintf('Changing ''%s'' to ''%s''\n',oldName,newName);
+            save([filename '.mat'],'oo');
+            delete([oldFilename '.mat'])
+        end
+    end
+end
+return
+   
+if 0
+% THE FIX: For 2019 files before April 3, when observer is Darshan or Sam
+% Coleman, then set observer to Sam Colman and experimenter to Darshan.
+        if ~ismember(oo(1).observer,{'Darshan' 'darshan'})
         % This block's observer valid, save for next block.
         if s(f)<1000
             observer=oo(1).observer;
@@ -93,8 +128,8 @@ for f=1:height(t)
                 oo(oi).observer=observer;
                 oo(oi).dataFilename=name;
             end
-                    save([name '.mat'],'oo');
-                    delete([oldName '.mat'])
+            save([name '.mat'],'oo');
+            delete([oldName '.mat'])
             fprintf('Change Darshan to %s\n',observer);
             if s(f)<1000
                 observer=oo(1).observer;
@@ -104,7 +139,6 @@ for f=1:height(t)
         end
     end
 end
-    
 if 0
 for nameCell=list
     name=nameCell{1};
