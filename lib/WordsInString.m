95553bb5e5f8f7d0e2983fd8c247f7c66509ac0f
denispelli <denis.pelli@nyu.edu>
2019-04-19 23:14:22 -0400

    new, parse string into words

---
+++ lib/WordsInString.m
@@ -0,0 +1,19 @@
+function words=WordsInString(string)
+% Parse text into a list of words.
+% denis.pelli@nyu.edu
+% April 18, 2019
+rem=string;
+words={};
+rem=strrep(rem,'"','');    % Remove double quote.
+rem=strrep(rem,' ''',' '); % Remove leading single quote.
+rem=strrep(rem,''' ',' '); % Remove trailing single quote.
+rem=strrep(rem,' -',' ');  % Remove leading dash.
+rem=strrep(rem,'- ',' ');  % Remove trailing dash.
+% Spare embedded apostrophes and dashes.
+while ~isempty(rem)
+    [words{end+1},rem]=strtok(rem,[newline char(255) ' Ž,:;.?!"0123456789?']);
+end
+if isempty(words{end})
+    words={words{1:end-1}};
+end
+end
