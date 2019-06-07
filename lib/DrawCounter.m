5041fc5011383980a76a95594e5652ea95209450
denispelli <denis.pelli@nyu.edu>
2019-06-06 22:12:45 -0400

    enhance DrawCounter
    copied new features from DrawCounter in NoiseDiscrimination.

--- lib/DrawCounter.m
+++ lib/DrawCounter.m
@@ -1,26 +1,70 @@
-function DrawCounter(oo)
-% DrawCounter(oo)
+function bounds=DrawCounter(o)
+% bounds=DrawCounter(o);
+% Use o.counterPlacement to specify alignment of counter in screenRect.
+% Displays "INVALID DATA" when in debugging mode. I.e. whenever
+% o.useFractionOfScreenToDebug~=0 or o.skipScreenCalibration=true.
+o=o(1); % Allow caller to send o or oo.
 
-% It would be great to display "INVALID DATA" when in debugging mode. I.e.
-% whenever o.useFractionOfScreenToDebug~=0 or o.skipScreenCalibration=true.
-
-global window scratchWindow
+global window scratchWindow scratchRect
 global blockTrial blockTrials
-% Display counter in lower right corner.
-if isempty(blockTrial)
-    message=sprintf('Block %d of %d.',...
-        oo(1).block,oo(1).blocksDesired);
-else
-    message=sprintf('Trial %d of %d. Block %d of %d.',...
-        blockTrial,blockTrials,oo(1).block,oo(1).blocksDesired);
+persistent counterSize counterBounds
+% Check arguments and globals.
+if isempty(window)
+    error('Require open window.');
 end
-counterSize=round(0.6*oo(1).textSize);
-oldTextSize=Screen('TextSize',window,counterSize);
+if Screen('WindowKind',window)==0
+    error('window is invalid');
+end
+if Screen('WindowKind',scratchWindow)==0
+    error('scratchWindow is invalid');
+end
+if isempty(scratchWindow)
+    [scratchWindow,scratchRect]=Screen('OpenOffscreenWindow',window);
+end
+% Compose the message.
+message='';
+if ~isempty(blockTrial)
+    message=sprintf('Trial %d of %d. ',blockTrial,blockTrials);
+end
+message=sprintf('%sBlock %d of %d.',message,o.block,o.blocksDesired);
+if isfield(o,'viewingDistanceCm')
+    message=sprintf('%s At %.0f cm.',message,o.viewingDistanceCm);
+end
+if o.useFractionOfScreenToDebug~=0 || o.skipScreenCalibration
+    message=['WARNING: This debugging mode invalidates data. ' message];
+end
+% Set size and font.
+if isfield(o,'textSize')
+    counterSize=round(0.6*o.textSize);
+else
+    counterSize=20;
+end
 Screen('TextSize',scratchWindow,counterSize);
+oldFont=Screen('TextFont',window,'Verdana');
+Screen('TextFont',scratchWindow,Screen('TextFont',window));
 counterBounds=TextBounds(scratchWindow,message,1);
-counterBounds=AlignRect(counterBounds,InsetRect(oo(1).stimulusRect,counterSize/4,counterSize/4),'right','bottom');
-white=WhiteIndex(window);
+r=Screen('Rect',window);
+% r=o.screenRect;
+if isfield(o,'stimulusRect')
+    r(3)=o.stimulusRect(3);
+end
+r=InsetRect(r,counterSize/4,counterSize/8);
+if ~isfield(o,'counterPlacement')
+	o.counterPlacement='bottomRight';
+end
+switch o.counterPlacement
+    case 'bottomRight'
+        counterBounds=AlignRect(counterBounds,r,'right','bottom');
+    case 'bottomLeft'
+        counterBounds=AlignRect(counterBounds,r,'left','bottom');
+    case 'bottomCenter'
+        counterBounds=AlignRect(counterBounds,r,'center','bottom');
+end
 black=BlackIndex(window);
-Screen('DrawText',window,message,counterBounds(1),counterBounds(4),black,white,1);
-Screen('TextSize',window,oldTextSize);
+oldTextSize=Screen('TextSize',window,counterSize);
+% Use whatever background is currently in use. Don't insist on white.
+Screen('DrawText',window,message,counterBounds(1),counterBounds(2),black,[]);
+Screen('TextFont',window,oldFont); % Restore.
+Screen('TextSize',window,oldTextSize); % Restore.
+bounds=counterBounds;
 end
