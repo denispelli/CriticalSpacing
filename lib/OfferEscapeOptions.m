1e58b393cc21f7e362b20bda06ce578c9a3ee4f3
denispelli <denis.pelli@nyu.edu>
2019-06-12 06:32:01 -0400

    polish
    no longer accept textMarginPix input argument.
    default font is now Verdana.
    TextSizeToFIt now returns two values: textSize and textLineLength.

--- lib/OfferEscapeOptions.m
+++ lib/OfferEscapeOptions.m
@@ -1,5 +1,5 @@
-function [quitExperiment,quitBlock,skipTrial]=OfferEscapeOptions(window,oo,textMarginPix)
-% [quitExperiment,quitBlock,skipTrial]=OfferEscapeOptions(window,oo,textMarginPix);
+function [quitExperiment,quitBlock,skipTrial]=OfferEscapeOptions(window,oo)
+% [quitExperiment,quitBlock,skipTrial]=OfferEscapeOptions(window,oo);
 % Copied from NoiseDiscrimination to CriticalSpacing, and modified just
 % enough to work.
 % May 1, 2019, denis.pelli@nyu.edu
@@ -14,21 +14,16 @@ if true
     o.speakEachLetter=false; % Not used in CriticalSpacing.
     o.gray1=255; % Not used in CriticalSpacing.
     if ~isfield(o,'textFont') || isempty(o.textFont)
-        o.textFont='Arial';
+        o.textFont='Verdana';
     end
-    if ~isfield(o,'textSize')
-        o.textSize=TextSizeToFit(window); % Set optimum text size in NoiseDiscrimination.
+    if ~isfield(o,'textSize') || ~isfield(o,'textLineLength')
+        [o.textSize,o.textLineLength]=TextSizeToFit(window,o.textFont); % Set optimum text size in NoiseDiscrimination.
+        fprintf('OfferEscapeOptions: recomputed textSize %d, textLineLength %d.\n',o.textSize,o.textLineLength);
     end
 end
 assert(isfield(o,'textFont') && ~isempty(o.textFont));
-if nargin<3
-    textMarginPix=2*o.textSize;
-end
 if o.speakEachLetter && o.useSpeech
     Speak('Escape');
-end
-if nargin<3
-    textMarginPix=2*o.textSize;
 end
 escapeKeyCode=KbName('ESCAPE');
 spaceKeyCode=KbName('space');
@@ -44,7 +39,7 @@ Screen('TextSize',window,o.textSize);
 black=0;
 Screen('Preference','TextAntiAliasing',0);
 Screen('FillRect',window);
-DrawCounter(oo);
+DrawCounter(o);
 % Set background color for DrawFormattedText.
 Screen('DrawText',window,' ',0,0,black,backgroundColor,1);
 if ~isfield(o,'isLastBlock')
@@ -63,8 +58,16 @@ end
 string=['You escaped. Any incomplete trial was canceled. ' ...
     'Hit ESCAPE again to quit the whole experiment. '...
     nextBlockMsg nextTrialMsg];
-DrawFormattedText(window,string,textMarginPix,1.5*o.textSize,...
-    black,oo(1).textLineLength,[],[],1.1);
+[x,y]=DrawFormattedText(window,string,...
+    2*o.textSize,2.5*o.textSize,black,...
+    o.textLineLength,[],[],1.3);
+% [x,y]=Screen('DrawText',window,...
+%     'ZZZPlease slowly type the name of the Experimenter who is sup.ZZZ',...
+%     0,y+1.3*o.textSize,black,[],1);
+% [x,y]=Screen('DrawText',window,...
+%     sprintf('textSize %d, textFont %s, textLineLength %d',...
+%     o.textSize,o.textFont,o.textLineLength),...
+%     0,y+2.6*o.textSize,black,[],1);
 Screen('Flip',window);
 answer=GetKeypress(...
     [spaceKeyCode returnKeyCode escapeKeyCode graveAccentKeyCode],...
@@ -73,7 +76,7 @@ quitExperiment=ismember(answer,[escapeChar,graveAccentChar]);
 quitBlock=ismember(answer,returnChar)||quitExperiment;
 skipTrial=ismember(answer,' ');
 if o.useSpeech
-    if quitExperiment || quitBlock && oo(1).isLastBlock
+    if quitExperiment || quitBlock && o.isLastBlock
         Speak('Done.');
     elseif quitBlock
         Speak('Proceeding to next block.');
