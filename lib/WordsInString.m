89bf1c05e3aae098a0d90059484f76c079d65214
Denis Pelli <denis.pelli@nyu.edu>
2019-07-16 11:47:35 -0400

    cosmetic

--- lib/WordsInString.m
+++ lib/WordsInString.m
@@ -1,9 +1,11 @@
 function words=WordsInString(string)
 % Parse text into a list of words.
 % denis.pelli@nyu.edu
-% April 18, 2019
-% May, 2019. Changed "newline" to char(10), because that symbol is not
-% defined in old versions of MATLAB. Introduced in R2016b.
+% July 16, 2019
+% In MATLAB 2018a, inexplicably, even though "newline" is present as a
+% built-in function, I am unable to call it from this routine. So I define
+% it myself. This must be a bug in MATLAB.
+newline=char(10); % Introduced in MATLAB 2016b.
 rem=string;
 words={};
 rem=strrep(rem,'"','');    % Remove double quote.
@@ -13,9 +15,9 @@ rem=strrep(rem,' -',' ');  % Remove leading dash.
 rem=strrep(rem,'- ',' ');  % Remove trailing dash.
 % Spare embedded apostrophes and dashes.
 while ~isempty(rem)
-    [words{end+1},rem]=strtok(rem,[char(10) char(255) ' ,:;.?!"0123456789?']);
+    [words{end+1},rem]=strtok(rem,[newline char(255) ' ,:;.?!"0123456789?']);
 end
-if isempty(words{end})
+if ~isempty(words) && isempty(words{end})
     words={words{1:end-1}};
 end
 end
