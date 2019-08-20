ab7e9c1ad3938a89fce39185c9c0e47683503cbb
Denis Pelli <denis.pelli@nyu.edu>
2019-08-20 17:27:26 -0400

    fix capitalization error
    fields of machine are now always capitalized, following the lead of WMIC.

--- utilities/ComputerModel.m
+++ utilities/ComputerModel.m
@@ -1,7 +1,7 @@
 function machine=ComputerModel
 switch computer
     case 'MACI64'
-% https://apple.stackexchange.com/questions/98080/can-a-macs-model-year-be-determined-with-a-terminal-command/98089
+        % https://apple.stackexchange.com/questions/98080/can-a-macs-model-year-be-determined-with-a-terminal-command/98089
         s = evalc(['!'...
             'curl -s https://support-sp.apple.com/sp/product?cc=$('...
             'system_profiler SPHardwareDataType '...
@@ -11,27 +11,39 @@ switch computer
         s=strrep(s,char(10),' '); % Change to space.
         s=strrep(s,char(13),' '); % Change to space.
         if s(end)==' '
-            s=s(1:end-1);
+            s=s(1:end-1); % Remove trailing space.
         end
-        machine.manufacturer='Apple Inc.';
-        machine.model=s;
+        machine.Model=s;
+        machine.Manufacturer='Apple Inc.';
     case 'PCWIN64'
-        s = evalc('!wmic computersystem get manufacturer, model');
-        % s=sprintf(['    ''Manufacturer  Model            \r'...
-        % '     Dell Inc.     Inspiron 5379    ']);
-        s=strrep(s,char(10),' '); % Change to space.
+        wmicString = evalc('!wmic computersystem get manufacturer, model');
+        % Here's a typical result:
+%         wmicString=sprintf(['    ''Manufacturer  Model            \r'...
+%         '     Dell Inc.     Inspiron 5379    ']);
+        s=strrep(wmicString,char(10),' '); % Change to space.
         s=strrep(s,char(13),' '); % Change to space.
-        s=regexprep(s,'  +',char(9)); % Tab.
+        s=regexprep(s,'  +',char(9)); % Change run of 2+ spaces to a tab.
         s=strrep(s,'''',''); % Remove stray quote.
         fields=split(s,char(9));
+        clear ok
         for i=1:length(fields)
             ok(i)=~isempty(fields{i});
         end
-        fields=fields(ok);
-        for i=1:2
-            machine.(fields{i})=fields{i+2};
+        fields=fields(ok); % Discard empty fields.
+        % The original had two columns: category and value. We've now got
+        % one long column with n categories followed by n values.
+        % We asked for Manufacturer and Model so n should be 2.
+        n=length(fields)/2;
+        for i=1:n
+            % Grab each field's name and value.
+            machine.(fields{i})=fields{i+n};
+        end
+        if ~isfield(machine,'Manufacturer') || isempty(machine.Manufacturer)...
+                || ~isfield(machine,'Model') || isempty(machine.Model)
+            wmicString
+            warning('Failed to retrieve Manufacturer and Model from WMIC.');
         end
     case 'GLNXA64'
-        machine.manufacturer='';
-        machine.model='linux';
+        machine.Manufacturer='';
+        machine.Model='linux';
 end
