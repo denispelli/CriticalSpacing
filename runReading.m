2556eb43529379e2a4da90200af2d2fbf7a48773
Denis Pelli <denis.pelli@nyu.edu>
2019-10-18 01:49:04 -0400

    trivial

--- runReading.m
+++ runReading.m
@@ -186,7 +186,7 @@ disp(t(:,{'block' 'experiment' 'conditionName' 'trialsDesired' 'targetFont'  ...
     'readSpacingDeg' 'eccentricityXYDeg' 'viewingDistanceCm'}));
 trials=sum([oo.trialsDesired]);
 fprintf('Total of %d trials, which may take about %.0f minutes. But reading trials take longer.\n',trials,trials/10);
-return
+% return
 
 %% Run.
 for block=1:length(ooo)
