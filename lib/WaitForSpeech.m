98eb4b536d15a5f0c43899df93432c9a6e03fbd1
Denis Pelli <denis.pelli@nyu.edu>
2020-01-10 01:45:15 -0500

    many enhancements

--- lib/WaitForSpeech.m
+++ lib/WaitForSpeech.m
@@ -1,7 +1,6 @@
-function timeSec=WaitForSpeech(action, arg, saveAudio, saveSpeechGraph, deviceName)
+function timeSec=WaitForSpeech(action, arg, saveAudio, deviceName)
 % WaitForSpeech('open' | 'close');
-% timeSec=WaitForSpeech('measure',name[,saveAudio=false]...
-%      [,saveSpeechGraph=false][,deviceName=[]]);
+% timeSec=WaitForSpeech('measure',name[,saveAudio=false][,deviceName=[]]);
 % WaitForSpeech('storeRating',rating);
 % The first argument specifies the desired "action", which can be 'open',
 % 'close', 'measure', or 'storeRating'. Actions 'open' and 'close' are used
@@ -11,6 +10,41 @@ function timeSec=WaitForSpeech(action, arg, saveAudio, saveSpeechGraph, deviceNa
 % Action 'storeRating' stores the observer's integer "rating" as a
 % filename. It is the same filename as used for the data, with the rating
 % appended.
+%
+% CAUTION: Under macOS, MATLAB requires the user's permission to use the
+% microphone. The user grants permission by going to
+%      System Preferences : Security & Privacy : Privacy
+% and clicking the checkbox next to "MATLAB". 
+% https://support.apple.com/guide/mac-help/control-access-to-your-microphone-on-mac-mchla1b1e1fe/mac
+% Note that each time you upgrade MATLAB, the user will again have to grant
+% permission to the new MATLAB application. If you call
+% WaitForSpeech('Open') while MATLAB does not yet have permission, then
+% either InitializePsychSound or PsychPortAudio puts up a modal dialog
+% asking the user to grant permission. Alas, if this comes up while we have
+% a full-screen window from Screen, then the dialog is displayed behind our
+% window and cannot be seen. MATLAB waits forever for the user to click the
+% invisible dialog box. I think the only way for the user to recover
+% control is to kill MATLAB.
+%
+% An easy way to avoid the hang up is to always call WaitForSpeech('open')
+% and 'close' once, very early, the first time CriticalSpacing runs, before
+% the Screen window is open. Then, if permission is needed, the dialog will
+% open and the user will see it and grant permission. The only downside is
+% that the roughly 1 s taken by these calls will be wasted in experiments
+% that never use the microphone. To avoid needlessly checking for
+% microphone permission we'd have to check not just this block for
+% o.task=='readAloud', but the whole experiment, because once the window is
+% open, we keep it open for the whole experiment.
+%
+% The two calls together take roughly 1 sec.
+% WaitForSpeech('open');
+% WaitForSpeech('close');
+%
+% Denis Pelli raised this as a Psychtoolbox issue in January 2020,
+% suggesting a fix inside InitializePsychSound or PsychPortAudio, whichever
+% opens the modal dialog.
+% https://github.com/Psychtoolbox-3/Psychtoolbox-3/issues/637
+% 
 % Written by Ziyi Zhang, December, 2019. Polished by denis.pelli@nyu.edu.
 
 if nargin<1
@@ -23,9 +57,6 @@ if nargin<3
     saveAudio=false;
 end
 if nargin<4
-    saveSpeechGraph=false;
-end
-if nargin<5
     deviceName='';
 end
 if isempty(deviceName)
@@ -44,6 +75,8 @@ maxWaitSec = 5;
 voiceTrigger = 0.1;
 % recordSecs is the duration recorded and stored on each trial.
 recordSecs = 1;
+% saveSpeechGraph has been deprecated, but left true on purpsoe
+saveSpeechGraph = true;
 switch action
     case 'open'
         if nargin>1
@@ -52,7 +85,7 @@ switch action
             error('Too many arguments. Only one allowed for open.');
         end
         AssertOpenGL; % We need Psychtoolbox 3.
-        InitializePsychSound;
+        InitializePsychSound(1); % The 1 requests low latentcy. Ignored except on Windows.
         % Open audio device 'device', with mode 2 (== Only audio capture),
         % and a required latencyclass of 1 == low-latency mode, with the
         % preferred default sampling frequency of the audio device, and 1
@@ -89,7 +122,8 @@ switch action
         fclose(fopen(sprintf('%s-%d.log',fileName,rating), 'w'));
         return
     case 'measure'
-        name=arg;
+        name=arg{1};
+        beginSec=arg{2};
         % Run code below.
     otherwise
         sca
@@ -137,7 +171,6 @@ while level < voiceTrigger
         WaitSecs(0.005);
     end
 end
-% disp('-- end recording --');  % Debug use
 if success
     % Find exact location of first above-threshold sample.
     idx = min(find(abs(audioData(1,:)) >= voiceTrigger));  %#ok<MXFND>
@@ -145,8 +178,8 @@ if success
     % Initialize our recordedAudio vector with captured data starting from
     % the sample that exceeded threshold:
     recordedAudio = audioData(:, idx:end);
-    if saveSpeechGraph
-        discardedAudio = [discardedAudio, audioData(:, 1:idx)];
+    if saveSpeechGraph && idx>1
+        discardedAudio = [discardedAudio, audioData(:, 1:idx-1)];
     end
     % Calculate speech onset time:
     timeSec = captureStartSec + ((offset + idx - 1) / freq);
@@ -160,6 +193,7 @@ if success
         WaitSecs(0.005);
     end
 end
+% disp('-- end recording --');  % Debug use
 % Stop capture.
 PsychPortAudio('Stop', pahandle);
 if ~success
@@ -182,8 +216,8 @@ if ~isempty(name) && (saveSpeechGraph || saveAudio)
 else
     fileName='';
 end
-% Plot wave
-if ~isempty(fileName) && saveSpeechGraph
+% Plot wave [deprecated, now plotted in grading.app]
+if ~isempty(fileName) && saveSpeechGraph && false
     f = figure('Visible', 'off');
     plotData = [discardedAudio, recordedAudio];
     plot((1:size(plotData, 2)) ./ freq, plotData(1, :), 'b');
@@ -196,9 +230,15 @@ if ~isempty(fileName) && saveSpeechGraph
     ylabel('Amplitude');
     saveas(f,[fileName '.fig']);
 end
-% Store recorded sound. Use fileName.
-if ~isempty(fileName) && saveAudio
+% Store recorded sound. Use fileName. [deprecated]
+if ~isempty(fileName) && saveAudio && false
     wavFileName = [fileName '.wav'];
     psychwavwrite(transpose(recordedAudio), freq, 16, wavFileName);
+end
+% Store recorded sound and other necessary information
+if ~isempty(fileName) && saveAudio
+    info = {[discardedAudio, recordedAudio], idxTotal, freq, voiceTrigger, ...
+            captureStartSec, beginSec}; %#ok
+    save([fileName, '.mat'], 'info');
 end
 end
