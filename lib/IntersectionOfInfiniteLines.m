3b72e8fe74399e9e71e12acd7217f04ea150bd2c
Denis Pelli <denis.pelli@nyu.edu>
2020-06-23 23:31:03 -0400

    very minor polishing

--- lib/IntersectionOfInfiniteLines.m
+++ lib/IntersectionOfInfiniteLines.m
@@ -8,36 +8,46 @@ function [x,y]=IntersectionOfInfiniteLines(lineA,lineB)
 % return NAN for both x and y. It is an error for either line segment to
 % have zero length.
 % 2016 by denis.pelli@nyu.edu
-bA=(lineA(2,1)-lineA(2,2))/(lineA(1,1)-lineA(1,2)); % dy/dx
+bA=diff(lineA(2,:))/diff(lineA(1,:)); % dy/dx
 aA=lineA(2,1)-bA*lineA(1,1);
-bB=(lineB(2,1)-lineB(2,2))/(lineB(1,1)-lineB(1,2)); % dy/dx
+bB=diff(lineB(2,:))/diff(lineB(1,:)); % dy/dx
 aB=lineB(2,1)-bB*lineB(1,1);
 if isnan(bA) || isnan(bB)
-   error('Both lines must have greater than zero length.');
+    lineA
+    lineB
+    x=nan;
+    y=nan;
+    if norm(diff(lineA'))
+        warning('lineA has zero length');
+    end
+    if norm(diff(lineB'))
+        warning('lineB has zero length');
+    end
+    return
 end
 if bA==bB
-   if aA==aB
-      x=lineA(1,1);
-      y=lineA(2,1);
-   else
-      x=nan;
-      y=nan;
-   end
-   return
+    if aA==aB
+        x=lineA(1,1);
+        y=lineA(2,1);
+    else
+        x=nan;
+        y=nan;
+    end
+    return
 end
 if isinf(bA)
-   x=lineA(1,1);
-   y=aB+bB*x;
-   return
+    x=lineA(1,1);
+    y=aB+bB*x;
+    return
 end
 if isinf(bB)
-   x=lineB(1,1);
-   y=aA+bA*x;
-   return
+    x=lineB(1,1);
+    y=aA+bA*x;
+    return
 end
 x=-(aA-aB)/(bA-bB);
 y=aA+bA*x;
 if ~isfinite(x) || ~isfinite(y)
-   warning('Infinite result (%.1f, %.1f) makes no sense.',x,y);
+    warning('Infinite result (%.1f, %.1f) makes no sense.',x,y);
 end
 
